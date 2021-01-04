53f5624c5e79440740eda5fd65dd0ff2035ddeb4
ryota-murakami <dojce1048@gmail.com>
2021-01-04 22:34:35 +0900

    tmp

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,44 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css'
 import { Header } from './Header'
+import { Post, Posts } from '../DataStructure'
 
 const App: React.FC = () => {
-  const [posts, setPosts] = useState()
+  const [posts, setPosts] = useState<Posts>([])
 
   useEffect(() => {
     async function fetchPosts() {
       try {
-        const { data } = await axios.get(
+        const { data } = await axios.get<Posts>(
           `${process.env.REACT_APP_API_ENDPOINT}/posts`
         )
-        // @ts-ignore
         setPosts(data)
       } catch (error) {
+        // eslint-disable-next-line no-console
         console.error(error)
       }
     }
     fetchPosts()
   })
-
-  console.log(posts)
-
   return (
     <div className="flex flex-col justify-between w-screen h-screen">
       <Header />
-      <main></main>
+      <main className="flex-grow px-4 py-4">
+        <ul className="flex flex-col justify-start">
+          {posts.map((post: Post, i) => {
+            return (
+              <>
+                <li key={i}>
+                  <span className="text-base text-gray-500">
+                    {new Date(parseInt(post.createdAt)).toLocaleDateString()}
+                  </span>
+                  <span className="space-x-8 text-lg">{post.title}</span>
+                </li>
+              </>
+            )
+          })}
+        </ul>
+      </main>
       <footer></footer>
     </div>
   )
