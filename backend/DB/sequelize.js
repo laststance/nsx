ea1e0b29a5938715093796ad41d5230ae25241bf
ryota-murakami <dojce1048@gmail.com>
2021-03-23 19:13:13 +0900

    acurate

--- backend/DB/sequelize.js
+++ backend/DB/sequelize.js
@@ -1,14 +1,16 @@
 import { Sequelize } from 'sequelize'
 
 // Option 2: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('wordpress', 'root', null, {
+const sequelize = new Sequelize('digital', 'root', 'rootpass', {
   host: 'localhost',
   dialect: 'mysql',
 })
 
 try {
   await sequelize.authenticate()
+  // eslint-disable-next-line no-console
   console.log('Connection has been established successfully.')
 } catch (error) {
+  // eslint-disable-next-line no-console
   console.error('Unable to connect to the database:', error)
 }
