37b6ea771afe953c453b0d95864306a1a5ccecf7
ryota-murakami <dojce1048@gmail.com>
2021-03-23 19:16:56 +0900

    up

--- backend/app.ts
+++ backend/app.ts
@@ -1,28 +1,9 @@
 import Express, { Request, Response } from 'express'
 import cors from 'cors'
-import { Sequelize } from 'sequelize'
-
 const app = Express()
 app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
-  // Option 2: Passing parameters separately (other dialects)
-  const sequelize = new Sequelize('wordpress', 'root', 'somewordpress', {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  })
-
-  async function connect() {
-    try {
-      await sequelize.authenticate()
-      console.log('Connection has been established successfully.')
-    } catch (error) {
-      console.error('Unable to connect to the database:', error)
-    }
-  }
-
-  connect()
-
   res.json({ msg: 'Express server is working!' })
 })
 
