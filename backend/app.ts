6b2697fb16cff1d2943115059d0a61a139d10763
ryota-murakami <dojce1048@gmail.com>
2021-04-22 19:04:10 +0900

    show jack trance

--- backend/app.ts
+++ backend/app.ts
@@ -7,4 +7,32 @@ app.get('/', (req: Request, res: Response) => {
   res.json({ msg: 'Express server is working!' })
 })
 
+app.get('/posts', (req: Request, res: Response) => {
+  // @Todo return hard coded Posts Json
+  res.json([
+    {
+      id: 1,
+      title: 'jack trance',
+      body: 'take me away to the post',
+      autor: {
+        id: 1,
+        name: 'kato',
+      },
+      createdAt: '1609683025845',
+      updatedAt: '1609683052915',
+    },
+    {
+      id: 2,
+      title: 'pot of greed',
+      body: 'next time down',
+      autor: {
+        id: 2,
+        name: 'odori',
+      },
+      createdAt: '1609683198726',
+      updatedAt: '1609684208831',
+    },
+  ])
+})
+
 export default app
