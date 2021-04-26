60f0bc5d1fcbb762b65e6d7bcf6b5f857a7231f5
ryota-murakami <dojce1048@gmail.com>
2021-04-27 05:00:09 +0900

    set header container width and x-axis margin

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header: React.FC = () => {
   return (
     <header className="w-full border-b border-gray-200 px-8">
-      <div className="w-full">
+      <div className="container mx-auto">
         <h1 className="font-bold text-2xl">Digital Strength</h1>
         <span className="text-sm text-gray-500">beyond the money limit</span>
       </div>
