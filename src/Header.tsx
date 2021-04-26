1906b51a265852efeba0fc9407bebfc9603fb062
ryota-murakami <dojce1048@gmail.com>
2021-04-27 06:42:09 +0900

    done break point setting

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Header: React.FC = () => {
   return (
-    <header className="w-full border-b border-gray-200 px-8">
+    <header className="w-full border-b border-gray-200">
       <div className="container mx-auto">
         <h1 className="font-bold text-2xl">Digital Strength</h1>
         <span className="text-sm text-gray-500">beyond the money limit</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,6 @@ module.exports = {
 
       xl: '1280px',
       // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
     },
     extend: {},
   },
