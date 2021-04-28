aefe896844ce76d1b7526c0f419da4b2498aa02b
ryota-murakami <dojce1048@gmail.com>
2021-04-29 04:40:33 +0900

    done main contaienr widh

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC<RouteComponentProps> = () => {
   return (
     <div className="flex flex-col justify-between w-screen h-screen">
       <Header />
-      <main className="flex-grow px-4 py-4">
+      <main className="container mx-auto flex-grow py-3">
         <ul className="flex flex-col justify-start">
           {posts.map((post: Post, i) => {
             return (
