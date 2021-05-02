a879a5ba5ed8e026e5e88501d9bcd68c6c49e879
ryota-murakami <dojce1048@gmail.com>
2021-05-03 07:51:20 +0900

    connect

---
+++ backend/DB/sequelize.ts
@@ -0,0 +1,25 @@
+import { Sequelize, DataTypes } from 'sequelize'
+
+// Option 2: Passing parameters separately (other dialects)
+const DB = new Sequelize('digital', 'strength', 'rootpass', {
+  dialect: 'mysql',
+})
+
+const Author = DB.define('author', {
+  name: {
+    type: DataTypes.STRING,
+  },
+})
+
+const Post = DB.define('post', {
+  title: {
+    type: DataTypes.STRING,
+  },
+  body: {
+    type: DataTypes.STRING,
+  },
+})
+
+Post.hasMany(Author)
+
+export default DB
--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,19 @@
 import app from './app'
+import DB from './DB/sequelize'
+
+async function connect() {
+  try {
+    await DB.authenticate()
+    console.log('Connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+connect()
+
+DB.sync({ force: true })
+  .then((res) => console.log(res))
+  .catch((res) => console.log(res))
 
 const port = 4000 || process.env.port
 
