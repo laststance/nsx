db4bf1a5c420bd16ff274b2fe69011d628fd5881
ryota-murakami <dojce1048@gmail.com>
2021-05-07 05:11:20 +0900

    fix lint

--- backend/server.ts
+++ backend/server.ts
@@ -4,16 +4,19 @@ import DB from './DB/sequelize'
 async function connect() {
   try {
     await DB.authenticate()
+    // eslint-disable-next-line no-console
     console.log('Connection has been established successfully.')
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.error('Unable to connect to the database:', error)
   }
 }
 connect()
 
-// ?????
 DB.sync({ force: true })
+  // eslint-disable-next-line no-console
   .then((res) => console.log(res))
+  // eslint-disable-next-line no-console
   .catch((res) => console.log(res))
 
 const port = 4000 || process.env.port
