5e1c979fd814c8a8d16bf79a91f7ec7b9584b004
ryota-murakami <dojce1048@gmail.com>
2021-05-07 15:55:23 +0900

    seed

--- backend/DB/importSeed.js
+++ backend/DB/importSeed.js
@@ -1,3 +1,6 @@
-const { Author } = require('./sequelize')
+const { Author, Post } = require('./sequelize')
 
 Author.create({ name: 'ryota' })
+
+Post.create({title: 'jack trance', body: 'take me away to the post'})
+Post.create({title: 'pot of greed', body: 'next time down'})
