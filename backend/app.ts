a9ab15f2e331184b94112a46c612c69f450c5e5e
ryota-murakami <dojce1048@gmail.com>
2021-05-09 16:59:24 +0900

    get sequelize model at frontend

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,6 @@
 import Express, { Request, Response } from 'express'
 import cors from 'cors'
+import { Post } from './DB/sequelize'
 const app = Express()
 app.use(cors())
 
@@ -7,32 +8,35 @@ app.get('/', (req: Request, res: Response) => {
   res.json({ msg: 'Express server is working!' })
 })
 
-app.get('/posts', (req: Request, res: Response) => {
+app.get('/posts', async (req: Request, res: Response) => {
+  //@ select all posts
+  const posts = await Post.findAll()
+  res.json(posts)
   // @Todo return hard coded Posts Json
-  res.json([
-    {
-      id: 1,
-      title: 'jack trance',
-      body: 'take me away to the post',
-      autor: {
-        id: 1,
-        name: 'kato',
-      },
-      createdAt: '1609683025845',
-      updatedAt: '1609683052915',
-    },
-    {
-      id: 2,
-      title: 'pot of greed',
-      body: 'next time down',
-      autor: {
-        id: 2,
-        name: 'odori',
-      },
-      createdAt: '1609683198726',
-      updatedAt: '1609684208831',
-    },
-  ])
+  // res.json([
+  //   {
+  //     id: 1,
+  //     title: 'jack trance',
+  //     body: 'take me away to the post',
+  //     autor: {
+  //       id: 1,
+  //       name: 'kato',
+  //     },
+  //     createdAt: '1609683025845',
+  //     updatedAt: '1609683052915',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'pot of greed',
+  //     body: 'next time down',
+  //     autor: {
+  //       id: 2,
+  //       name: 'odori',
+  //     },
+  //     createdAt: '1609683198726',
+  //     updatedAt: '1609684208831',
+  //   },
+  // ])
 })
 
 app.get('/post/:id', (req: Request, res: Response) => {
