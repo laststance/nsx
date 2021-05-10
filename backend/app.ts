9b37030f4b1aa5a64875d48255041192fe50570b
ryota-murakami <dojce1048@gmail.com>
2021-05-10 12:01:12 +0900

    remove comment

--- backend/app.ts
+++ backend/app.ts
@@ -12,31 +12,6 @@ app.get('/posts', async (req: Request, res: Response) => {
   //@ select all posts
   const posts = await Post.findAll({ include: { model: Author, as: 'author' } })
   res.json(posts)
-  // @Todo return hard coded Posts Json
-  // res.json([
-  //   {
-  //     id: 1,
-  //     title: 'jack trance',
-  //     body: 'take me away to the post',
-  //     autor: {
-  //       id: 1,
-  //       name: 'kato',
-  //     },
-  //     createdAt: '1609683025845',
-  //     updatedAt: '1609683052915',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'pot of greed',
-  //     body: 'next time down',
-  //     autor: {
-  //       id: 2,
-  //       name: 'odori',
-  //     },
-  //     createdAt: '1609683198726',
-  //     updatedAt: '1609684208831',
-  //   },
-  // ])
 })
 
 app.get('/post/:id', (req: Request, res: Response) => {
