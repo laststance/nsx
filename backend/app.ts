89fdd87cd2d7c151df8efc94268d9d558986d556
ryota-murakami <dojce1048@gmail.com>
2021-05-11 08:25:23 +0900

    add type

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,15 @@
 import Express, { Request, Response } from 'express'
 import cors from 'cors'
+import { Model } from 'sequelize'
 import { Post, Author } from './DB/sequelize'
+// @TODO find more straightforwardway to use DataStructure's Posts interface.
+import { Post as PostType } from '../DataStructure'
 const app = Express()
 app.use(cors())
 
-app.get('/posts', async (req: Request, res: Response) => {
+app.get('/posts', async (req: Request, res: Response<Model<PostType>[]>) => {
   //@ select all posts
-  const posts = await Post.findAll({
+  const posts = await Post.findAll<Model<PostType>>({
     include: { model: Author, as: 'author' },
     attributes: { exclude: ['authorId'] },
   })
