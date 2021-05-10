5c687860f8a8b19988ece4c4e4517a4e8b85abd9
ryota-murakami <dojce1048@gmail.com>
2021-05-10 09:13:51 +0900

    color

--- backend/DB/connectTest.js
+++ backend/DB/connectTest.js
@@ -1,15 +1,18 @@
 // connect to DB and sync table definition
 const { DB } = require('./sequelize')
+const chalk = require('chalk')
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const connectTest = async () => {
   try {
     await DB.authenticate()
     // eslint-disable-next-line no-console
-    console.log('Connection has been established successfully.')
+    console.log(
+      chalk.green.bold('Connection has been established successfully.')
+    )
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.error('Unable to connect to the database:', error)
+    console.error(chalk.red.bold('Unable to connect to the database:'), error)
   }
 }
 
