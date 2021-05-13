c5802b38173f9e7d7a8002ca3a88e9b37b6f9ce1
ryota-murakami <dojce1048@gmail.com>
2021-05-13 20:49:49 +0900

    impl

--- backend/app.ts
+++ backend/app.ts
@@ -8,45 +8,22 @@ const app = Express()
 app.use(cors())
 
 app.get('/posts', async (req: Request, res: Response<Model<PostType>[]>) => {
-  //@ select all posts
   const posts = await Post.findAll<Model<PostType>>({
     include: { model: Author, as: 'author' },
     attributes: { exclude: ['authorId'] },
   })
+
   res.json(posts)
 })
 
-app.get('/post/:id', (req: Request, res: Response) => {
-  if (req.params.id === '1') {
-    res.json({
-      id: 1,
-      title: 'jack trance',
-      body: 'take me away to the post',
-      autor: {
-        id: 1,
-        name: 'kato',
-      },
-      createdAt: '1609683025845',
-      updatedAt: '1609683052915',
-    })
-  } else if (req.params.id === '2') {
-    res.json({
-      id: 2,
-      title: 'pot of greed',
-      body: 'next time down',
-      autor: {
-        id: 2,
-        name: 'odori',
-      },
-      createdAt: '1609683198726',
-      updatedAt: '1609684208831',
-    })
-  } else {
-    res.json({
-      code: 500,
-      message: 'unexpected error',
-    })
-  }
+app.get('/post/:id', async (req: Request, res: Response) => {
+  const post = await Post.findOne({
+    where: { id: req.params.id },
+    include: { model: Author, as: 'author' },
+    attributes: { exclude: ['authorId'] },
+  })
+
+  res.json(post)
 })
 
 export default app
