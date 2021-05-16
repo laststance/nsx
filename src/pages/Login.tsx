eaf7b61a71f8be1d8efeb64e70595bc420f5db8f
ryota-murakami <dojce1048@gmail.com>
2021-05-16 17:10:01 +0900

    handle change

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -83,7 +83,7 @@ const Login: React.FC<RouteComponentProps> = () => {
               className="shadow bg-blue-500 hover:bg-blue-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
               type="submit"
             >
-              Login
+              Submit
             </button>
           </div>
         </div>
