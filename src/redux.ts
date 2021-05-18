8182676b32c700febd132f7d46fcaed382fc98fe
ryota-murakami <dojce1048@gmail.com>
2021-05-18 11:58:38 +0900

    redux

--- src/redux.ts
+++ src/redux.ts
@@ -1,7 +1,17 @@
 import { Action, Reducer, createStore } from 'redux'
 
 export interface ReduxState {
+  isSignup: boolean
   isLogin: boolean
+}
+
+export interface SignupAction extends Action<'SIGNUP'> {
+  type: 'SIGNUP'
+}
+
+export interface CloseSignupSnackbarAction
+  extends Action<'CLOSE_SIGINUP_SNACKBAR'> {
+  type: 'CLOSE_SIGINUP_SNACKBAR'
 }
 
 export interface LoginAction extends Action<'LOGIN'> {
@@ -12,10 +22,14 @@ export interface LogoutAction extends Action<'LOGOUT'> {
   type: 'LOGOUT'
 }
 
-type MyAppAction = LoginAction | LogoutAction
+type MyAppAction =
+  | LoginAction
+  | LogoutAction
+  | SignupAction
+  | CloseSignupSnackbarAction
 
 const reducer: Reducer<ReduxState | undefined, MyAppAction> = (
-  state = { isLogin: false },
+  state = { isSignup: false, isLogin: false },
   action
 ) => {
   switch (action.type) {
@@ -24,7 +38,10 @@ const reducer: Reducer<ReduxState | undefined, MyAppAction> = (
 
     case 'LOGOUT':
       return { ...state, isLogin: false }
-
+    case 'SIGNUP':
+      return { ...state, isSignup: true }
+    case 'CLOSE_SIGINUP_SNACKBAR':
+      return { ...state, isSignup: false }
     default:
       return state
   }
