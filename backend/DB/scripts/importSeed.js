4032bc1cdf7ccca75fdbfc0d0ef1c7f1330c598b
ryota-murakami <dojce1048@gmail.com>
2021-05-18 13:58:36 +0900

    seed

--- backend/DB/scripts/importSeed.js
+++ backend/DB/scripts/importSeed.js
@@ -1,15 +1,22 @@
 #!/usr/bin/env node
 const { Post, Author } = require('../sequelize')
 
-Post.create(
-  {
+async function main() {
+  const ryota = await Author.create({
+    name: 'ryota',
+    password: '$2b$10$KX/AIDa3Yh6IMGevfxEhl.yRPCgR3wXgoFO5Ni1iP2rN.2Z.MP.oW',
+  })
+
+  Post.create({
     title: 'jack trance',
     body: 'take me away to the post',
-    author: { name: 'ryota' },
-  },
-  { include: { model: Author, as: 'author' } }
-)
-Post.create(
-  { title: 'pot of greed', body: 'next time down', author: { name: 'ryota' } },
-  { include: { model: Author, as: 'author' } }
-)
+    authorId: ryota.id,
+  })
+  Post.create({
+    title: 'pot of greed',
+    body: 'next time down',
+    authorId: ryota.id,
+  })
+}
+
+main()
