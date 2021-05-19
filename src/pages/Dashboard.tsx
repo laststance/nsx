120c556e3787e9e74361e89aae0248c598a325fa
ryota-murakami <dojce1048@gmail.com>
2021-05-19 18:35:34 +0900

    comment

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RouteComponentProps } from '@reach/router'
+import Layout from '../components/Layout'
 
-const Dashboard: React.FC<RouteComponentProps> = () => <div>dashbord</div>
+const Dashboard: React.FC<RouteComponentProps> = () => {
+  useEffect(() => {
+    // @TODO impl popup and close redux state
+  }, [])
+
+  return <Layout>dashbord</Layout>
+}
 
 export default Dashboard
