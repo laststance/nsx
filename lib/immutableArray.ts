7d282d7afbba8068fb68c410df0371249dae1cfc
ryota-murakami <dojce1048@gmail.com>
2021-05-20 06:06:21 +0900

    lib

--- lib/immutableArray.ts
+++ lib/immutableArray.ts
@@ -1,11 +1,16 @@
 // @see https://ultimatecourses.com/blog/all-about-immutable-arrays-and-objects-in-javascript
 
-type PopReturnType = [lastItem: unknown, removedLastItemArray: Array<unknown>]
+type PopReturnType = [lastItem: unknown, removedLastItremArray: Array<unknown>]
+type ShiftReturnType = [
+  firstItem: unknown,
+  removedFirstItemArray: Array<unknown>
+]
 
 export interface ImmutableArrayInterface {
   push(existing: Array<unknown>, newItem: unknown): Array<unknown>
   unshift(existing: Array<unknown>, newItem: unknown): Array<unknown>
   pop(arr: Array<unknown>): PopReturnType
+  shift(arr: Array<unknown>): ShiftReturnType
 }
 
 const ImmutableArray: ImmutableArrayInterface = {
@@ -21,6 +26,12 @@ const ImmutableArray: ImmutableArrayInterface = {
     const lastItem = arr[arr.length - 1]
     const removedLastItemArray = arr.slice(0, arr.length - 1)
     return [lastItem, removedLastItemArray]
+  },
+
+  shift(arr: Array<unknown>): ShiftReturnType {
+    const firstItem = arr[0]
+    const removedFirstItemArray = arr.slice(1)
+    return [firstItem, removedFirstItemArray]
   },
 }
 
