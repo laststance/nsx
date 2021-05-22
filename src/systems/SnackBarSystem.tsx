7a2f9e4db5bb8017a5d1575d257f904bd3de2f7f
ryota-murakami <dojce1048@gmail.com>
2021-05-22 16:46:05 +0900

    snackbar

--- src/systems/SnackBarSystem.tsx
+++ src/systems/SnackBarSystem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { DequeueSnackbarAction, ReduxState } from '../redux'
 import { SnackBarMessage } from '../../DataStructure'
@@ -26,15 +26,23 @@ interface Props {
 
 const SnackBar: React.FC<Props> = ({ message }) => {
   const dispatch: Dispatch<DequeueSnackbarAction> = useDispatch()
+  const [opacity, setOpacity] = useState('opacity-0')
   useEffect(() => {
-    setTimeout(
-      () => dispatch({ type: 'DEQUEUE_SNACKBAR_MESSAGE' }),
-      9000000000000000000000
-    )
+    setOpacity('opacity-100')
+
+    const timerId = setTimeout(() => {
+      setOpacity('opacity-0')
+      dispatch({ type: 'DEQUEUE_SNACKBAR_MESSAGE' })
+    }, 3000)
+
+    return () => clearTimeout(timerId)
   }, [dispatch])
+
   return (
-    <div className="z-10 absolute bg-green-400 w-40 h-24">
-      <p>{message}</p>
+    <div
+      className={`flex justify-center items-center z-10 py-2 px-4 absolute bg-green-500 top-10 right-10 rounded-xl button transition-opacity ${opacity}`}
+    >
+      <p className="text-white text-lg uppercase font-medium">{message}</p>
     </div>
   )
 }
