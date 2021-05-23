b9a8af91fcc6ae9c406dec7c55c71dc092d09d8e
ryota-murakami <dojce1048@gmail.com>
2021-05-24 04:22:08 +0900

    todo

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
 
-const Create: React.FC<RouteComponentProps> = () => <div>Create</div>
+const Create: React.FC<RouteComponentProps> = () => {
+  // @TODO editor
+  // @TODO submit
+  return <div>Create</div>
+}
 
 export default Create
