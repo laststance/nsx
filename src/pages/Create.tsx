34e2cebe3a377206847897d7fbc4c8031693dba5
ryota-murakami <dojce1048@gmail.com>
2021-05-26 02:03:48 +0900

    front base

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RouteComponentProps } from '@reach/router'
+import axios from 'axios'
 import Layout from '../components/Layout'
 
 const Create: React.FC<RouteComponentProps> = () => {
-  // @TODO editor
-  // @TODO submit
+  const [title, setTitle] = useState<string | undefined>('')
+  const [body, setBody] = useState<string | undefined>('')
+
+  function handleChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    cb: React.Dispatch<React.SetStateAction<string | undefined>>
+  ): void {
+    e.preventDefault()
+    cb(e.target.value)
+  }
+
+  async function execCreate() {
+    try {
+      const { status } = await axios.post(
+        `${process.env.REACT_APP_API_ENDPOINT}/create`,
+        {
+          title,
+          body,
+        }
+      )
+
+      if (status === 201) {
+        // @TODO success
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+    }
+  }
+
   return (
     <Layout className="flex flex-col justify-start">
-      <input type="text" className="mt-3" />
-      <textarea className="w-full h-60 mt-3" />
+      <input
+        type="text"
+        className="mt-3"
+        value={title}
+        onChange={(e) => handleChange(e, setTitle)}
+      />
+      <textarea
+        className="w-full h-60 mt-3"
+        value={body}
+        onChange={(e) => handleChange(e, setBody)}
+      />
       <div className="flex gap-4 justify-end">
-        <button className="mt-3 shadow bg-green-400 hover:bg-green-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+        <button
+          onClick={execCreate}
+          className="mt-3 shadow bg-green-400 hover:bg-green-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+        >
           Submit
         </button>
       </div>
