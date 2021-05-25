be95ec88311a12e2ab8bc2f547d11dfaca0082fc
ryota-murakami <dojce1048@gmail.com>
2021-05-26 02:54:10 +0900

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const Footer: React.FC = () => <div>Footer</div>
+const Footer: React.FC = () => (
+  <div className="container px-6 mx-auto mt-16">
+    <div className="flex flex-col items-center border-t border-gray-200">
+      <div className="py-6 text-center sm:w-2/3">
+        <p className="text-sm text-gray-600">© 2021 by Ryota Murakami</p>
+      </div>
+    </div>
+  </div>
+)
 
 export default Footer
