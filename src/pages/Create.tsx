8b240a6ab3cc4c02845239ac0e6f0b5f06aaa0f9
ryota-murakami <dojce1048@gmail.com>
2021-05-26 01:16:23 +0900

    base

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
+import Layout from '../components/Layout'
 
 const Create: React.FC<RouteComponentProps> = () => {
   // @TODO editor
   // @TODO submit
-  return <div>Create</div>
+  return (
+    <Layout className="flex flex-col justify-start">
+      <input type="text" className="mt-3" />
+      <textarea className="w-full h-60 mt-3" />
+      <div className="flex gap-4 justify-end">
+        <button className="mt-3 shadow bg-green-400 hover:bg-green-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+          Submit
+        </button>
+      </div>
+    </Layout>
+  )
 }
 
 export default Create
