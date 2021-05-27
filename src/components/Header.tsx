3fc35e5db30a7bea82fe2e63fe7b4871e5971721
ryota-murakami <dojce1048@gmail.com>
2021-05-27 15:01:44 +0900

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,14 @@ import { Link } from '@reach/router'
 const Header: React.FC<RouteComponentProps> = () => {
   return (
     <header data-cy="header" className="w-full border-b border-gray-200">
-      <div className="container mx-auto">
+      <div className="container mx-auto py-3 flex gap-4 items-center">
         <Link to="/">
           <h1 className="font-bold text-2xl">Digital Strength</h1>
         </Link>
-        <span className="text-sm text-gray-500">beyond the boundary</span>
+        <p className="text-base text-gray-500">
+          I'm blogging what today I learned about web technology for beyond the
+          elite boundary.
+        </p>
       </div>
     </header>
   )
