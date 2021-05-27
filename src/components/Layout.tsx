b9a7e44add76a4b66469a401d4d2dc66d927f0a6
ryota-murakami <dojce1048@gmail.com>
2021-05-27 11:23:21 +0900

    utils

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,26 +1,22 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { concatSelecor } from '../utils'
 
 interface Props {
   className?: string
 }
 
-function getContainerStyle(className: Props['className']): string {
-  const base = 'container mx-auto flex-grow py-3'
-
-  if (className) {
-    return base + ' ' + className
-  } else {
-    return base
-  }
-}
-
 const Layout: React.FC<Props> = ({ children, className }) => {
+  let containerStyle = 'container mx-auto flex-grow py-3'
+  if (className) {
+    containerStyle = concatSelecor(containerStyle, className)
+  }
+
   return (
     <div className="flex flex-col justify-between w-screen h-screen">
       <Header />
-      <main className={getContainerStyle(className)}>{children}</main>
+      <main className={containerStyle}>{children}</main>
       <Footer />
     </div>
   )
