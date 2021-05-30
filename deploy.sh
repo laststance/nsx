8651d802cfe5dbbcee5262efceefb2dd1c5b367a
ryota-murakami <dojce1048@gmail.com>
2021-05-31 00:44:00 +0900

    tmp

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 LOCAL_ROOT=$(dirname $0)
 REMOTE_ROOT=/root/digital-strength
 
-ssh root@digitalstrength.dev 'cd digital-strength | rm -rf server_build | rm -rf build'
-scp -r $LOCAL_ROOT/build digitalstrength.dev:$REMOTE_ROOT/build
-scp -r $LOCAL_ROOT/server_build digitalstrength.dev:$REMOTE_ROOT/server_build
+ssh root@digitalstrength.dev 'cd digital-strength; rm -rfv server_build; rm -rfv build;'
+scp -rv $LOCAL_ROOT/build digitalstrength.dev:$REMOTE_ROOT/build
+scp -rv $LOCAL_ROOT/server_build digitalstrength.dev:$REMOTE_ROOT/server_build
