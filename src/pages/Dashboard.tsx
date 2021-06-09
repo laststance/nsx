33d999425a1506c7efae3495561187919d27f375
ryota-murakami <dojce1048@gmail.com>
2021-06-09 19:44:12 +0900

    margin

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, RouteComponentProps } from '@reach/router'
 import Layout from '../components/Layout'
+import Button from '../components/Button'
 import { Post } from '../../DataStructure'
 import usePostList from '../hooks/usePostList'
 
@@ -13,20 +14,19 @@ const Dashboard: React.FC<RouteComponentProps> = () => {
       <ul className="flex flex-col justify-start">
         {posts.map((post: Post, i) => {
           return (
-            <li key={i} className="flex justify-between items-center">
-              <Link
-                to={`/post/${post.id}`}
-                className="flex items-center space-x-2"
-              >
+            <li key={i} className="flex justify-between items-center space-y-3">
+              <Link to={`/post/${post.id}`} className="flex items-center">
                 <div className="text-base text-gray-500">
-                  {new Date(parseInt(post.createdAt)).toLocaleDateString()}
+                  {new Date(post.createdAt).toLocaleDateString('en-US', {
+                    year: '2-digit',
+                    month: '2-digit',
+                    day: '2-digit',
+                  })}
                 </div>
                 <div className="text-base">{post.title}</div>
               </Link>
               <Link to={`edit/${post.id}`}>
-                <button className="shadow focus:shadow-outline focus:outline-none py-2 px-4 rounded">
-                  Edit
-                </button>
+                <Button className="text-gray-500">Edit</Button>
               </Link>
             </li>
           )
