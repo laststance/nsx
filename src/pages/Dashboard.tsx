a84d73953c70177d0ca360af1e7a187f09dc3a98
ryota-murakami <dojce1048@gmail.com>
2021-06-10 18:34:53 +0900

    refetch

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, RouteComponentProps } from '@reach/router'
 import Layout from '../components/PageContainer'
 import Button from '../elements/Button'
-import { Post } from '../../DataStructure'
-import usePostList from '../hooks/usePostList'
-import axios from 'axios'
+import { Post, Posts } from '../../DataStructure'
+import axios, { AxiosError } from 'axios'
 import { Dispatch } from 'redux'
 import { useDispatch } from 'react-redux'
 import { EnqueueSnackbarAction } from '../redux'
 
 const Dashboard: React.FC<RouteComponentProps> = () => {
   const dispatch: Dispatch<EnqueueSnackbarAction> = useDispatch()
-  const { posts, axiosError } = usePostList()
+  const [posts, setPosts] = useState<Posts>([])
+  // for display network error message
+  const [axiosError, setAxiosError] = useState<AxiosError>()
+
+  async function fetchPosts() {
+    try {
+      const { data } = await axios.get<Posts>(
+        `${process.env.REACT_APP_API_ENDPOINT}/posts`
+      )
+      setPosts(data)
+      // @ts-ignore
+    } catch (error: AxiosError) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+      setAxiosError(error)
+    }
+  }
+
   async function handleDelete(id: Post['id']) {
     try {
       const { status } = await axios.delete(
@@ -23,6 +39,7 @@ const Dashboard: React.FC<RouteComponentProps> = () => {
           type: 'ENQUEUE_SNACKBAR_MESSAGE',
           payload: { message: 'Delete Successful!', color: 'green' },
         })
+        fetchPosts()
       } else {
         dispatch({
           type: 'ENQUEUE_SNACKBAR_MESSAGE',
@@ -39,6 +56,10 @@ const Dashboard: React.FC<RouteComponentProps> = () => {
       })
     }
   }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   return (
     <Layout className="flex flex-col justify-start">
