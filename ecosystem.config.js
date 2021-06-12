dcae17517e8c51de001de55f6e53391cdfd84cb8
ryota-murakami <dojce1048@gmail.com>
2021-06-12 23:37:06 +0900

    up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      script: './server_build/server.js',
+      script: './server_build/server/index.js',
       env: {
         NODE_ENV: 'production',
       },
