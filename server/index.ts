1953204dadade195851c15f970a741ab4c09a575
ryota-murakami <dojce1048@gmail.com>
2021-06-12 23:49:52 +0900

    up

--- server/index.ts
+++ server/index.ts
@@ -135,7 +135,12 @@ router.post('/api/update', async (req: Request, res: Response) => {
  * ==============================================
  */
 if (isProd) {
-  app.use(express.static(path.join(__dirname, '../build')))
+  app.use(
+    vhost(
+      'digitalstrength.dev',
+      express.static(path.join(__dirname, '../build'))
+    )
+  )
 
   app.use(vhost('api.digitalstrength.dev', router))
 
