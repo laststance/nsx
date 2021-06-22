e823f692ac016218f0ac013340c30d913e1b61e5
ryota-murakami <dojce1048@gmail.com>
2021-06-23 02:34:21 +0900

    commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,38 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const COMMAND_DELAY = 0
+
+for (const command of [
+  'visit',
+  'click',
+  'trigger',
+  'type',
+  'clear',
+  'reload',
+  'contains',
+]) {
+  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+    const origVal = originalFn(...args)
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(origVal)
+      }, COMMAND_DELAY)
+    })
+  })
+}
+
+Cypress.Commands.add('login', (name = 'ryota', password = 'popcoon') => {
+  return cy.request('POST', '/api/login', {
+    name,
+    password,
+  })
+})
+
+Cypress.Commands.add('logout', () => {
+  cy.clearCookies()
+  cy.clearLocalStorage()
+  return cy.visit('http://localhost:3000')
+})
+
+Cypress.Commands.add('resetDB', () => {
+  cy.exec('yarn db:drop && db:seed')
+})
