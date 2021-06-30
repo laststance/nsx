d5d6a0f955b93d7643d950261a40bd78ae171540
ryota-murakami <dojce1048@gmail.com>
2021-06-30 10:16:30 +0900

    about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,20 +6,23 @@ const About: React.FC<RouteComponentProps> = () => (
   <Container>
     <h1 className="text-3xl pt-4 pb-6">About</h1>
     <section className="text-xl leading-8">
-      weiojfioawefjiowaefjiowajfioajeofiaw
+      This Blog built for logging roughly what todaly I learned everyday by{' '}
+      <a
+        href="https://ryota-murakami.github.io/"
+        target="_blank"
+        rel="noreferrer"
+        className="text-gray-600 hover:text-gray-400"
+      >
+        Ryota Murakami
+      </a>
+      .
       <br />
-      weiojfioawefjiowaefjiowajfioajeofiaw
+      I'm writing very broken note here that what today I
+      learned/read/acomplished/struggled whatever.
       <br />
-      weiojfioawefjiowaefjiowajfioajeofiaw
       <br />
-      weiojfioawefjiowaefjiowajfioajeofiaw
-      <br />
-      weiojfioawefjiowaefjiowajfioajeofiaw
-      <br />
-      weiojfioawefjiowaefjiowajfioajeofiaw
-      <br />
-      weiojfioawefjiowaefjiowajfioajeofiaw
-      <br />
+      It's save me from depression and remind whatever I known me when I feel
+      like no grow up as a Web Engineer.
     </section>
   </Container>
 )
