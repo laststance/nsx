324448fd81096eeeb84c1f9c7d1578b3c1fabd0b
ryota-murakami <dojce1048@gmail.com>
2021-07-07 09:46:12 +0900

    css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,14 @@ import { Link } from '@reach/router'
 const Header: React.FC<RouteComponentProps> = () => {
   return (
     <header data-cy="header" className="w-full border-b border-gray-200">
-      <div className="container mx-auto grid grid-cols-3 grid-rows-1 gap-4 content-center items-center my-4">
-        <Link to="/">
+      <div className="container mx-auto flex items-center content-center gap-2 my-4">
+        <Link to="/" className="col-auto">
           <h1 className="font-bold text-2xl">Digital Strength β</h1>
         </Link>
-        <p className="text-base text-gray-500">Just putting Today I Learned</p>
-        <div className="justify-end flex justify-end">
+        <p className="text-base text-gray-500 justify-self-start">
+          Just putting Today I Learned
+        </p>
+        <div className="flex-grow flex justify-end">
           <Link to="/about">
             <div className="hover:text-gray-300">about</div>
           </Link>
