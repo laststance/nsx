7c5f3e4ee8dc94571d0bd77614be75951f72a272
ryota-murakami <dojce1048@gmail.com>
2021-07-13 02:10:42 +0900

    static

--- server/reverseProxy.ts
+++ server/reverseProxy.ts
@@ -2,10 +2,6 @@ import fs from 'fs'
 // @ts-ignore there is no @types/redbird
 import redbird from 'redbird'
 
-// @TODO redbird.listen('digitalstrength.dev', redbird.static('./build') , 443)
-// redbird.proxy('digitalstrength.dev/api', http://localhost:4534)
-// @TODO https://github.com/OptimalBits/redbird#https-example
-
 const privatekey = fs.readFileSync(
   '/etc/letsencrypt/live/digitalstrength.dev/privkey.pem',
   'utf8'
@@ -18,3 +14,21 @@ const ca = fs.readFileSync(
   '/etc/letsencrypt/live/digitalstrength.dev/chain.pem',
   'utf8'
 )
+
+redbird.register('digitalstrength.dev', 'http://localhost:4328', {
+  ssl: {
+    port: 443,
+    key: privatekey,
+    cert: certificate,
+    ca: ca,
+  },
+})
+
+redbird.register('digitalstrength.dev/api', 'http://localhost:4534', {
+  ssl: {
+    port: 443,
+    key: privatekey,
+    cert: certificate,
+    ca: ca,
+  },
+})
--- server/static.ts
+++ server/static.ts
@@ -1,1 +1,19 @@
-// @TODO implement express static
+import path from 'path'
+import http from 'http'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const app = express()
+// @ts-ignore
+app.use(bodyParser())
+app.use(cors())
+
+app.use('/', express.static(path.join(__dirname, '../../build')))
+
+const staticServer = http.createServer(app)
+
+staticServer.listen(4328, () => {
+  // eslint-disable-next-line no-console
+  console.log('Static Server running on port 4328')
+})
