71ed4940019e5439b0d932c45bf9edb810a44273
ryota-murakami <dojce1048@gmail.com>
2021-07-13 13:23:42 +0900

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   apps: [
     {
+      name: 'api',
       script: './server_build/server/api.js',
       env: {
         NODE_ENV: 'production',
       },
     },
+    { name: 'static', script: './server_build/server/static.js' },
+    { name: 'reverseProxy', script: './server_build/server/reverseProxy.js' },
   ],
 }
