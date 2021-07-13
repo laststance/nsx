17ef33139d9794b8fb4412909446a570fb1fad03
ryota-murakami <dojce1048@gmail.com>
2021-07-13 18:33:30 +0900

    prod stage 1

--- server/reverseProxy.ts
+++ server/reverseProxy.ts
@@ -1,34 +1,19 @@
-import fs from 'fs'
 // @ts-ignore there is no @types/redbird
-import redbird from 'redbird'
+const redbird = new require('redbird')({
+  port: 80,
 
-const privatekey = fs.readFileSync(
-  '/etc/letsencrypt/live/digitalstrength.dev/privkey.pem',
-  'utf8'
-)
-const certificate = fs.readFileSync(
-  '/etc/letsencrypt/live/digitalstrength.dev/cert.pem',
-  'utf8'
-)
-const ca = fs.readFileSync(
-  '/etc/letsencrypt/live/digitalstrength.dev/chain.pem',
-  'utf8'
-)
-
-redbird.register('digitalstrength.dev', 'http://localhost:4328', {
+  // Specify filenames to default SSL certificates (in case SNI is not supported by the
+  // user's browser)
   ssl: {
     port: 443,
-    key: privatekey,
-    cert: certificate,
-    ca: ca,
+    key: '/etc/letsencrypt/live/digitalstrength.dev/privkey.pem',
+    cert: '/etc/letsencrypt/live/digitalstrength.dev/cert.pem',
+    ca: '/etc/letsencrypt/live/digitalstrength.dev/chain.pem',
   },
 })
 
-redbird.register('digitalstrength.dev/api', 'http://localhost:4534', {
-  ssl: {
-    port: 443,
-    key: privatekey,
-    cert: certificate,
-    ca: ca,
-  },
+redbird.register('digitalstrength.dev', 'http://localhost:4328', { ssl: true })
+
+redbird.register('digitalstrength.dev/api', 'http://localhost:4534/api', {
+  ssl: true,
 })
