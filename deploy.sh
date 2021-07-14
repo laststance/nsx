36dced7634da454e518ceb37f3056f4d4e8bf01b
ryota-murakami <dojce1048@gmail.com>
2021-07-14 12:47:47 +0900

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,18 @@ usage() { echo "Usage: $0 [-s] [-c]" 1>&2; exit 1; }
 while getopts ":sc" opt; do
     case "${opt}" in
         s)
-            ssh root@digitalstrength.dev 'cd digital-strength; rm -rfv server_build;'
+            ssh root@digitalstrength.dev 'cd digital-strength; rm -rf server_build;'
             scp -rv $LOCAL_ROOT/server_build digitalstrength.dev:$REMOTE_ROOT/server_build
             exit 0
             ;;
         c)
-            ssh root@digitalstrength.dev 'cd digital-strength; rm -rfv build;'
+            ssh root@digitalstrength.dev 'cd digital-strength; rm -rf build;'
             scp -rv $LOCAL_ROOT/build digitalstrength.dev:$REMOTE_ROOT/build
             exit 0
             ;;
     esac
 done
 
-ssh root@digitalstrength.dev 'cd digital-strength; rm -rfv server_build; rm -rfv build;'
+ssh root@digitalstrength.dev 'cd digital-strength; rm -rf server_build; rm -rf build;'
 scp -rv $LOCAL_ROOT/build digitalstrength.dev:$REMOTE_ROOT/build
 scp -rv $LOCAL_ROOT/server_build digitalstrength.dev:$REMOTE_ROOT/server_build
