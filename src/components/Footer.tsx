afe4461ecc6b87006f46b7137b1fdc8dbc38e059
ryota-murakami <dojce1048@gmail.com>
2021-07-25 04:13:53 +0900

    footer border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Footer: React.FC = () => (
-  <div className="container px-6 mx-auto mt-16">
-    <div className="flex flex-col items-center border-t border-gray-200">
+  <footer>
+    <div className="w-full flex flex-col items-center px-6 border-t mt-16 border-gray-200">
       <div className="py-6 text-center sm:w-2/3">
         <p className="text-sm text-gray-600">© 2021 by Ryota Murakami</p>
       </div>
     </div>
-  </div>
+  </footer>
 )
 
 export default React.memo(Footer, () => true)
