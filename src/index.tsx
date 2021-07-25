97ef6cca6103b081a957fa3db177d90f8f5410d1
ryota-murakami <dojce1048@gmail.com>
2021-07-25 09:30:54 +0900

    remove serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider as ReduxProvider } from 'react-redux'
 import './index.css'
-import * as serviceWorker from './serviceWorker'
 import Routes from './systems/Routes'
 import SnackBarSystem from './systems/SnackBarSystem'
 import ErrorBoundary from './systems/ErrorBoundary'
@@ -29,8 +28,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
