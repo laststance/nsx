24aad721c09c999b601bf20d8bd915f9454a04cc
ryota-murakami <dojce1048@gmail.com>
2021-07-30 21:56:13 +0900

    remove pro

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { Profiler } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider as ReduxProvider } from 'react-redux'
 import './index.css'
@@ -17,56 +17,12 @@ if (window.localStorage.getItem('login') === 'true') {
   store.dispatch(login(author))
 }
 
-let queue: {
-  id: any
-  phase: any
-  actualDuration: any
-  baseDuration: any
-  startTime: any
-  commitTime: any
-  interactions: any
-}[] = []
-// sendProfileQueue every 5 seconds
-setInterval(sendProfileQueue, 5000)
-function onRenderCallback(
-  id: any,
-  phase: any,
-  actualDuration: any,
-  baseDuration: any,
-  startTime: any,
-  commitTime: any,
-  interactions: any
-) {
-  queue.push({
-    id,
-    phase,
-    actualDuration,
-    baseDuration,
-    startTime,
-    commitTime,
-    interactions,
-  })
-}
-function sendProfileQueue() {
-  if (!queue.length) {
-    return Promise.resolve()
-  }
-  const queueToSend = [...queue]
-  queue = []
-  // here's where we'd actually make the server call to send the queueToSend
-  // data to our backend...
-  console.info('sending profile queue', queueToSend)
-  return Promise.resolve()
-}
-
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
       <ReduxProvider store={store}>
         <SnackBarSystem />
-        <Profiler id="App" onRender={onRenderCallback}>
-          <Routes />
-        </Profiler>
+        <Routes />
       </ReduxProvider>
     </ErrorBoundary>
   </React.StrictMode>,
