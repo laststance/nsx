8e68ed5049937d54bc9187dc210348464589792e
ryota-murakami <dojce1048@gmail.com>
2021-07-30 23:18:30 +0900

    loding style

--- src/elements/Loading.tsx
+++ src/elements/Loading.tsx
@@ -4,8 +4,8 @@ import { CircleLoader } from 'react-spinners'
 export const Loading = memo(
   () => {
     return (
-      <div className="w-full h-full flex justify-center items-center">
-        <CircleLoader size={150} color="#36D7B7" />
+      <div className="w-screen h-screen flex justify-center items-center">
+        <CircleLoader size={250} color="#36D7B7" />
       </div>
     )
   },
