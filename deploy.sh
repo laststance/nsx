5970b468197ac5a92212c4c7c4ac79eaec5801dc
ryota-murakami <dojce1048@gmail.com>
2021-07-30 15:16:28 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
 LOCAL_ROOT=$(dirname $0)
-REMOTE_ROOT=/root/digital-strength
+REMOTE_ROOT=/root/digital-strength/
 
 usage() { echo "Usage: $0 [-s] [-c]" 1>&2; exit 1; }
 
 while getopts ":sc" opt; do
     case "${opt}" in
         s)
-            ssh root@digitalstrength.dev 'cd digital-strength; rm -rf server_build;'
-            scp -r $LOCAL_ROOT/server_build digitalstrength.dev:$REMOTE_ROOT/server_build
-            scp $LOCAL_ROOT/ecosystem.config.js digitalstrength.dev:$REMOTE_ROOT/ecosystem.config.js
+            rsync -avzhc $LOCAL_ROOT/server_build digitalstrength.dev:$REMOTE_ROOT
+            rsync -avzhc $LOCAL_ROOT/ecosystem.config.js digitalstrength.dev:$REMOTE_ROOT
             exit 0
             ;;
-        c)
-            ssh root@digitalstrength.dev 'cd digital-strength; rm -rf build;'
-            scp -r $LOCAL_ROOT/build digitalstrength.dev:$REMOTE_ROOT/build
+        f)
+            rsync -avzhc $LOCAL_ROOT/build digitalstrength.dev:$REMOTE_ROOT
             exit 0
             ;;
     esac
 done
 
-ssh root@digitalstrength.dev 'cd digital-strength; rm -rf server_build; rm -rf build;'
-scp -r $LOCAL_ROOT/build digitalstrength.dev:$REMOTE_ROOT/build
-scp -r $LOCAL_ROOT/server_build digitalstrength.dev:$REMOTE_ROOT/server_build
-scp $LOCAL_ROOT/ecosystem.config.js digitalstrength.dev:$REMOTE_ROOT/ecosystem.config.js
+rsync -avzhc $LOCAL_ROOT/build digitalstrength.dev:$REMOTE_ROOT
+rsync -avzhc $LOCAL_ROOT/server_build digitalstrength.dev:$REMOTE_ROOT
+rsync -avzhc $LOCAL_ROOT/ecosystem.config.js digitalstrength.dev:$REMOTE_ROOT
