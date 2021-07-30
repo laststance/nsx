32b8521a18963f5e563304ef3b5d045838b1c90f
ryota-murakami <dojce1048@gmail.com>
2021-07-30 23:41:18 +0900

    Auth

--- src/systems/AuthBoundary.tsx
+++ src/systems/AuthBoundary.tsx
@@ -1,12 +1,12 @@
-import React, { PropsWithChildren } from 'react'
+import React, { memo } from 'react'
 import { Redirect, RouteComponentProps } from '@reach/router'
 import { useAppSelector } from '../redux/hooks'
 import { selectLogin } from '../redux/adminSlice'
 
-const AuthBoundary: React.FC<RouteComponentProps> = ({ children }) => {
+const AuthBoundary = memo<RouteComponentProps>(({ children }) => {
   const login = useAppSelector(selectLogin)
 
   return login ? <>{children}</> : <Redirect to="/login" noThrow />
-}
+})
 
-export default React.memo<PropsWithChildren<RouteComponentProps>>(AuthBoundary)
+export default AuthBoundary
