5c17afdbdb97bd682a5d843ec7fd66525f119e93
ryota-murakami <dojce1048@gmail.com>
2021-08-01 04:18:55 +0900

    sequelize init

---
+++ models/author.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class author extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  author.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'author',
+  });
+  return author;
+};
