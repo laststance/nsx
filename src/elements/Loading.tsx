f2e837c88240d5350e41497ec0d7f107a983407b
ryota-murakami <dojce1048@gmail.com>
2021-08-01 19:36:06 +0900

    spinner color

--- src/elements/Loading.tsx
+++ src/elements/Loading.tsx
@@ -5,7 +5,7 @@ export const Loading = memo(
   () => {
     return (
       <div className="w-full h-full flex justify-center items-center">
-        <CircleLoader size={250} color="#36D7B7" />
+        <CircleLoader size={250} color="#9CA3AF" />
       </div>
     )
   },
