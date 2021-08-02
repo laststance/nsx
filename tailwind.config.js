f14a43fad42891c34f4f0cfe847947a36fb53347
ryota-murakami <dojce1048@gmail.com>
2021-08-03 01:21:22 +0900

    remove wasted config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,6 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    screens: {
-      sm: '640px',
-      // => @media (min-width: 640px) { ... }
-
-      md: '768px',
-      // => @media (min-width: 768px) { ... }
-    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
