833c4e81d650769fc4d4420cab0dd2e8dc2b1258
ryota-murakami <dojce1048@gmail.com>
2021-08-06 05:12:23 +0900

    refacotr Post: clean code & clear comments:

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -20,17 +20,21 @@ interface RouterParam {
   postId: PostType['id']
 }
 
+// This is cumtom <a/> tag component for pass <ReactMarkdown compoment={{a}} /> props
 const a: React.FC = (props) => (
   // eslint-disable-next-line jsx-a11y/anchor-has-content
   <a {...props} target="_blank" className="text-blue-700"></a>
 )
 
+// This is cumtom <code/> tag component for pass <ReactMarkdown compoment={{code}} /> props
 const code = lazy(
+  // <code/> depends on heavy hintaxhilight library so we lazyload for purpose of reduce bundle chunk size
   () =>
     // @ts-ignore @TODO react-syntax-highlighter issue
     import(/* webpackChunkName: "code" */ '../elements/code')
 )
 
+// we only load <code/> if blog post containing Markdown for purpose of reduce bundle chunk size
 const getCustomComponents = (data: { body: string | string[] }) => {
   return data.body.includes('```') ? { a, code } : { a }
 }
@@ -49,51 +53,52 @@ const Post: React.FC<RouteComponentProps<RouterParam>> = ({ postId }) => {
     if (error) dispatch(enque({ message: error.toString(), color: 'red' }))
   }, [dispatch, error])
 
+  if (isLoading || data === undefined) {
+    return (
+      <Layout data-cy="postPage">
+        <Loading />
+      </Layout>
+    )
+  }
+
   return (
     <Layout data-cy="postPage">
       <Suspense fallback={<Loading />}>
-        {isLoading ? (
-          <Loading />
-        ) : (
-          data && (
-            <>
-              <Helmet>
-                <meta
-                  name="description"
-                  content={truncateString(data.body, 20)}
-                />
-                <meta property="og:title" content={data.title} />
-                <meta
-                  property="og:description"
-                  content={truncateString(data.body, 20)}
-                />
-                <meta property="og:type" content="article" />
-                <meta property="og:url" content="https://digitalstrength.dev" />
-                <meta
-                  name="og:image"
-                  content="https://digitalstrength.dev/ogp.png"
-                />
-                <meta name="twitter:card" content="summary" />
-                <meta name="twitter:site" content="@malloc007" />
-                <meta
-                  name="twitter:image"
-                  content="https://digitalstrength.dev/ogp.png"
-                />
-                <title>{data.title}</title>
-              </Helmet>
-              <h1 className="text-2xl pt-4 pb-6 font-semibold">{data.title}</h1>
-              <ReactMarkdown
-                components={getCustomComponents(data)}
-                /* @ts-ignore lib index.d.ts missmatch between "@types/node@16.4.12" and "rehype-raw@6.0.0" */
-                rehypePlugins={[rehypeRaw]}
-                remarkPlugins={[breaks, gfm]}
-                className="prose prose-lg"
-              >
-                {data.body}
-              </ReactMarkdown>
-            </>
-          )
-        )}
+        {/* Suspence for lazyload expesive <code /> component */}
+        <>
+          <Helmet>
+            <meta name="description" content={truncateString(data.body, 20)} />
+            <meta property="og:title" content={data.title} />
+            <meta
+              property="og:description"
+              content={truncateString(data.body, 20)}
+            />
+            <meta property="og:type" content="article" />
+            <meta property="og:url" content="https://digitalstrength.dev" />
+            <meta
+              name="og:image"
+              content="https://digitalstrength.dev/ogp.png"
+            />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content="@malloc007" />
+            <meta
+              name="twitter:image"
+              content="https://digitalstrength.dev/ogp.png"
+            />
+            <title>{data.title}</title>
+          </Helmet>
+          <h1 className="text-2xl pt-4 pb-6 font-semibold">{data.title}</h1>
+          <ReactMarkdown
+            components={getCustomComponents(data)}
+            /* @ts-ignore lib index.d.ts missmatch between "@types/node@16.4.12" and "rehype-raw@6.0.0" */
+            rehypePlugins={[rehypeRaw]}
+            remarkPlugins={[breaks, gfm]}
+            className="prose prose-lg"
+          >
+            {data.body}
+          </ReactMarkdown>
+        </>
+        )
         {login && (
           <div className="mt-16">
             <Link to={`/dashboard/edit/${postId}`}>
