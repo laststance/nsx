3904b67f3a72f3f45f52f8d8df0da9aff4e0e235
ryota-murakami <dojce1048@gmail.com>
2021-08-10 19:38:46 +0900

    remove unused server-js-deploy.sh

--- server-js-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOCAL_ROOT=$(dirname $0)
-REMOTE_ROOT=/root/digital-strength/
-
-
-rsync -avzhc $LOCAL_ROOT/server.js digitalstrength.dev:$REMOTE_ROOT
-rsync -avzhc $LOCAL_ROOT/db digitalstrength.dev:$REMOTE_ROOT
-rsync -avzhc $LOCAL_ROOT/ecosystem.config.js digitalstrength.dev:$REMOTE_ROOT
