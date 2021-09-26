2c09b8496ae6f566a7807f5fccea9b93c92dc8e3
ryota-murakami <dojce1048@gmail.com>
2021-09-26 23:42:27 +0900

    add ga

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider as ReduxProvider } from 'react-redux'
+import type { Metric } from 'web-vitals'
 
 import './index.css'
 import type { Author } from '../types'
@@ -62,8 +63,18 @@ if (rootElement.hasChildNodes()) {
   ReactDOM.render(<App />, rootElement)
 }
 
+function sendToAnalytics({ id, name, value }: Metric) {
+  ga('send', 'event', {
+    eventCategory: 'Web Vitals',
+    eventAction: name,
+    eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    eventLabel: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate
+  })
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // eslint-disable-next-line no-console
-reportWebVitals(console.info)
+reportWebVitals(sendToAnalytics)
