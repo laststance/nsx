fe290b8532ef9cdef72de0dbf08bc2cfdfdb6928
ryota-murakami <dojce1048@gmail.com>
2021-09-27 10:58:30 +0900

    tbd

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider as ReduxProvider } from 'react-redux'
-import type { Metric } from 'web-vitals'
+// import type { Metric } from 'web-vitals'
 
 import './index.css'
 import type { Author } from '../types'
@@ -9,7 +9,7 @@ import type { Author } from '../types'
 import { login } from './redux/adminSlice'
 import { API } from './redux/API'
 import { store } from './redux/store'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
 import ErrorBoundary from './systems/ErrorBoundary'
 import Routes from './systems/Routes'
 import SnackBarSystem from './systems/SnackBarSystem'
@@ -63,18 +63,18 @@ if (rootElement.hasChildNodes()) {
   ReactDOM.render(<App />, rootElement)
 }
 
-function sendToAnalytics({ id, name, value }: Metric) {
-  ga('send', 'event', {
-    eventCategory: 'Web Vitals',
-    eventAction: name,
-    eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-    eventLabel: id, // id unique to current page load
-    nonInteraction: true, // avoids affecting bounce rate
-  })
-}
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// eslint-disable-next-line no-console
-reportWebVitals(sendToAnalytics)
+// function sendToAnalytics({ id, name, value }: Metric) {
+//   ga('send', 'event', {
+//     eventCategory: 'Web Vitals',
+//     eventAction: name,
+//     eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+//     eventLabel: id, // id unique to current page load
+//     nonInteraction: true, // avoids affecting bounce rate
+//   })
+// }
+//
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // eslint-disable-next-line no-console
+// reportWebVitals(sendToAnalytics)
