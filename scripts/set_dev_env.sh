43d96828e6ad8e024021653729aec83ff5d94eca
ryota-murakami <dojce1048@gmail.com>
2021-10-05 00:05:28 +0900

    up

--- scripts/set_dev_env.sh
+++ scripts/set_dev_env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 
-sed -i -e "s/#REACT_APP_API_ENDPOINT=http:\/\/localhost:3000\/api\//REACT_APP_API_ENDPOINT=http:\/\/localhost:3000\/api\//" $ROOT_DIR/.env
-sed -i -e "s/REACT_APP_API_ENDPOINT=https:\/\/digitalstrength.dev\/api\//#REACT_APP_API_ENDPOINT=https:\/\/digitalstrength.dev\/api\//" $ROOT_DIR/.env
+sed -i -e "s/REACT_APP_API_ENDPOINT=https:\/\/digitalstrength.dev\/api\//REACT_APP_API_ENDPOINT=http:\/\/localhost:3000\/api\//" $ROOT_DIR/.env
 #sed -i -e "s/REACT_APP_ENABLE_LOGIN=false/REACT_APP_ENABLE_LOGIN=true/" $ROOT_DIR/.env
 #sed -i -e "s/REACT_APP_ENABLE_SIGNUP=false/REACT_APP_ENABLE_SIGNUP=true/" $ROOT_DIR/.env
--- scripts/set_prod_env.sh
+++ scripts/set_prod_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
+echo $ROOT_DIR
 
-sed -i -e "s/#REACT_APP_API_ENDPOINT=https:\/\/digitalstrength.dev\/api\//REACT_APP_API_ENDPOINT=https:\/\/digitalstrength.dev\/api\//" $ROOT_DIR/.env
-sed -i -e "s/REACT_APP_API_ENDPOINT=http:\/\/localhost:3000\/api\//#REACT_APP_API_ENDPOINT=http:\/\/localhost:3000\/api\//" $ROOT_DIR/.env
+sed -i -e "s/REACT_APP_API_ENDPOINT=http:\/\/localhost:3000\/api\//REACT_APP_API_ENDPOINT=https:\/\/digitalstrength.dev\/api\//g" $ROOT_DIR/.env
 #sed -i -e "s/REACT_APP_ENABLE_SIGNUP=true/REACT_APP_ENABLE_SIGNUP=false/" $ROOT_DIR/.env
