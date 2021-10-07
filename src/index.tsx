55366c2910720bdbf935f884be24fdf86c7fe4c1
ryota-murakami <dojce1048@gmail.com>
2021-10-07 23:43:34 +0900

    up

--- src/index.tsx
+++ src/index.tsx
@@ -68,7 +68,7 @@ if (process.env.NODE_ENV === 'production') {
 
   function sendToAnalytics({ id, name, value }: Metric) {
     ga('send', 'event', {
-      eventCategory: 'Web Vitals',
+      eventCategory: 'Web-Vitals',
       eventAction: name,
       eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
       eventLabel: id, // id unique to current page load
@@ -78,6 +78,5 @@ if (process.env.NODE_ENV === 'production') {
   // If you want to start measuring performance in your app, pass a function
   // to log results (for example: reportWebVitals(console.log))
   // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  // eslint-disable-next-line no-console
   reportWebVitals(sendToAnalytics)
 }
