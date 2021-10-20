029a4ff7e3c2988713dc1da3eb03c040f3983221
ryota-murakami <dojce1048@gmail.com>
2021-10-21 01:06:23 +0900

    up

--- src/elements/ArrowButton.tsx
+++ src/elements/ArrowButton.tsx
@@ -12,7 +12,7 @@ const ArrowButton: React.FC<
   ArrowButtonProps & ButtonHTMLAttributes<HTMLButtonElement>
 > = ({ direction, ...rest }) => (
   <button
-    className="w-14 disabled:opacity-30 disabled:cursor-not-allowed focus:outline-none hover:text-gray-400 hover:border-gray-400 flex items-center justify-center h-10 border border-gray-500 rounded"
+    className="w-14 disabled:opacity-30 disabled:cursor-not-allowed focus:outline-none hover:text-gray-400 hover:border-gray-200 hover:bg-gray-100 flex items-center justify-center h-10 border border-gray-500 rounded"
     {...rest}
   >
     {direction === 'left' && <ArrowLeft />}
