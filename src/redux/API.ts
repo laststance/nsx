7cb2134906e45b040f4e6e76c291e2dbc72bcd1d
ryota-murakami <dojce1048@gmail.com>
2021-11-26 08:43:25 +0900

    up

--- src/redux/API.ts
+++ src/redux/API.ts
@@ -21,7 +21,7 @@ export const API = createApi({
     },
     fetchFn: (requestInfo: RequestInfo, ...rest) => fetch(requestInfo, ...rest),
   }),
-  keepUnusedDataFor: 10,
+  keepUnusedDataFor: 180,
   tagTypes: ['Posts'],
   endpoints: (builder) => ({
     fetchPostList: builder.query<PostListResponce, PostListRequestParam>({
