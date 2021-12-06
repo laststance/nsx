dfe844988156a8085e1ba36727eef9887694771d
ryota-murakami <dojce1048@gmail.com>
2021-12-07 06:14:31 +0900

    memo ArrowButton

--- src/elements/ArrowButton.tsx
+++ src/elements/ArrowButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import type { ButtonHTMLAttributes } from 'react'
 
 import ArrowLeft from './icons/ArrowLeft'
@@ -10,7 +10,7 @@ interface ArrowButtonProps {
 
 const ArrowButton: React.FC<
   ArrowButtonProps & ButtonHTMLAttributes<HTMLButtonElement>
-> = ({ direction, ...rest }) => (
+> = memo(({ direction, ...rest }) => (
   <button
     className="w-14 disabled:opacity-30 disabled:cursor-not-allowed focus:outline-none hover:text-gray-400 hover:border-gray-200 hover:bg-gray-100 flex items-center justify-center h-10 border border-gray-500 rounded"
     {...rest}
@@ -18,5 +18,7 @@ const ArrowButton: React.FC<
     {direction === 'left' && <ArrowLeft />}
     {direction === 'right' && <ArrowRight />}
   </button>
-)
+))
+ArrowButton.displayName = 'ArrowButton'
+
 export default ArrowButton
