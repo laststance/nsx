9e40b1252d923991b68253ba328da1a0c1317b64
ryota-murakami <dojce1048@gmail.com>
2021-12-07 06:54:58 +0900

    tweek

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -23,7 +23,7 @@ const PostPage: React.FC<RouteComponentProps<RouterParam>> = memo(
 
     const { data, isLoading, error } = API.endpoints.fetchPost.useQuery(
       parseInt(postId),
-      { skip: cache !== undefined } /* No Cache then Real Fetch */
+      /* No Cache then Real Fetch */ { skip: cache !== undefined }
     )
 
     /* Render with Cache */
