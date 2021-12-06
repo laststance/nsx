35500c1ef4228faa284eb6a3414fad1e56b28ae7
ryota-murakami <dojce1048@gmail.com>
2021-12-07 06:27:11 +0900

    optimize SnackBar

--- src/elements/SnackBar.tsx
+++ src/elements/SnackBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 
 import { useAppDispatch } from '../redux/hooks'
 import type { SnackBarMessage } from '../redux/snackbarSlice'
@@ -9,37 +9,40 @@ interface Props {
   color: SnackBarMessage['color']
 }
 
-// @TODO refined animation and apperence
-export const SnackBar: React.FC<Props> = ({ message, color }) => {
-  const dispatch = useAppDispatch()
-  const [opacity, setOpacity] = useState('opacity-0')
-  const bgColor = (color: string): string => {
-    if (color === 'green') {
+export const SnackBar: React.FC<Props> = memo(
+  ({ message, color }) => {
+    const dispatch = useAppDispatch()
+    const [opacity, setOpacity] = useState('opacity-0')
+    const bgColor = (color: string): string => {
+      if (color === 'green') {
+        return 'bg-green-500'
+      } else if (color === 'red') {
+        return 'bg-red-500'
+      }
       return 'bg-green-500'
-    } else if (color === 'red') {
-      return 'bg-red-500'
     }
-    return 'bg-green-500'
-  }
 
-  useEffect(() => {
-    setOpacity('opacity-100')
+    useEffect(() => {
+      setOpacity('opacity-100')
 
-    const timerId = setTimeout(() => {
-      setOpacity('opacity-0')
-      dispatch(dequeSnackbar())
-    }, 3000)
+      const timerId = setTimeout(() => {
+        setOpacity('opacity-0')
+        dispatch(dequeSnackbar())
+      }, 3000)
 
-    return () => clearTimeout(timerId)
-  }, [dispatch])
+      return () => clearTimeout(timerId)
+    }, [dispatch])
 
-  return (
-    <div
-      className={`max-w-xl flex justify-center items-center ${bgColor(
-        color
-      )} z-10 py-2 px-4 absolute top-10 right-10 rounded-xl button transition-opacity ${opacity}`}
-    >
-      <p className="text-lg font-medium text-white uppercase">{message}</p>
-    </div>
-  )
-}
+    return (
+      <div
+        className={`max-w-xl flex justify-center items-center ${bgColor(
+          color
+        )} z-10 py-2 px-4 absolute top-10 right-10 rounded-xl button transition-opacity ${opacity}`}
+      >
+        <p className="text-lg font-medium text-white uppercase">{message}</p>
+      </div>
+    )
+  },
+  () => true
+)
+SnackBar.displayName = 'SnackBar'
