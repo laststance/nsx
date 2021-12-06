eaa340298e6c0eaee31c6ce6b65c5c4bcbc83aa2
ryota-murakami <dojce1048@gmail.com>
2021-12-07 06:22:54 +0900

    optimize code

--- src/elements/code.tsx
+++ src/elements/code.tsx
@@ -2,22 +2,24 @@
  * These componets are used to react-markdown library.
  * Refer https://github.com/remarkjs/react-markdown#appendix-b-components
  */
-import React from 'react'
-import type CodeComponent from 'react-markdown'
+import React, { memo } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 
 // @ts-ignore
-const code: typeof CodeComponent = ({ inline, className, children }) => {
-  const match = /language-(\w+)/.exec(className || '')
-  return !inline && match ? (
-    <SyntaxHighlighter
-      language={match[1]}
-      PreTag="div"
-      children={String(children).replace(/\n$/, '')}
-    />
-  ) : (
-    <code className={className}>{children}</code>
-  )
-}
+const code: React.FC = memo(({ inline, className, children }) => { /* eslint-disable-line prettier/prettier */
+    const match = /language-(\w+)/.exec(className || '')
+    return !inline && match ? (
+      <SyntaxHighlighter
+        language={match[1]}
+        PreTag="div"
+        children={String(children).replace(/\n$/, '')}
+      />
+    ) : (
+      <code className={className}>{children}</code>
+    )
+  },
+  () => true
+)
+code.displayName = 'code'
 
 export default code
