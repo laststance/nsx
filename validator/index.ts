aa41ffa13a880541e98e7f846874cf97e12040e7
ryota-murakami <dojce1048@gmail.com>
2021-12-20 21:17:02 +0900

    ref

--- validator/index.ts
+++ validator/index.ts
@@ -3,19 +3,22 @@ import { define, object } from 'superstruct'
 
 import { assertCast } from '../src/lib/assertCast'
 
-export const name = define('name', (value): Result => {
+export const name = define<Author['name']>('name', (value): Result => {
   assertCast<string>(value)
   return value.trim().length > 3 && value.trim().length < 100
     ? true
     : 'name should be 3~100 characters'
 })
 
-export const password = define('password', (value): Result => {
-  assertCast<string>(value)
-  return value.trim().length > 6 && value.trim().length < 100
-    ? true
-    : 'password must be at least 6 characters long'
-})
+export const password = define<Author['password']>(
+  'password',
+  (value): Result => {
+    assertCast<string>(value)
+    return value.trim().length > 6 && value.trim().length < 100
+      ? true
+      : 'password must be at least 6 characters long'
+  }
+)
 
 export const signupFormVallidator = object({
   name: name,
