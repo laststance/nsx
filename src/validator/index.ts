fe9f6176b57d0a46527e4bc0cc3ef1820f3aad0d
ryota-murakami <dojce1048@gmail.com>
2021-12-20 17:13:19 +0900

    up

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -3,19 +3,17 @@ import { define, object } from 'superstruct'
 
 import { assertCast } from '../lib/assertCast'
 
-// @TODO add certain rules
 export const name = define('name', (value): Result => {
   assertCast<string>(value)
-  return value.trim().length > 1 && value.trim().length < 100
+  return value.trim().length > 3 && value.trim().length < 100
     ? true
-    : 'name should be 2~100 characters'
+    : 'name should be 3~100 characters'
 })
 
-// @TODO add certain rules
 export const password = define('password', (value): Result => {
   assertCast<string>(value)
-  if (value.trim().length < 2)
-    return 'password must be at least 2 characters long'
+  if (value.trim().length > 6 && value.trim().length < 100)
+    return 'password must be at least 6 characters long'
 
   return true
 })
