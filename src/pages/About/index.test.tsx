6f5fa099e716691446ddf4507346cb1219ffd892
ryota-murakami <dojce1048@gmail.com>
2022-01-20 08:31:58 +0900

    about test

---
+++ src/pages/About/index.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import TestRenderer from '../../lib/TestRenderer'
+
+import About from './'
+
+test('should render About', () => {
+  const { container } = TestRenderer(<About />)
+  expect(container).toHaveTextContent('by Ryota Murakami')
+})
