8127df803f0c9fcc6bb373465144eb923aedc17e
ryota-murakami <dojce1048@gmail.com>
2022-01-26 15:48:22 +0900

    fresh

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,34 +1,37 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  'stories': [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-postcss"
+  'addons': [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss'
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "storybook-builder-vite"
+  'core': {
+    'builder': 'webpack5'
   },
-  "webpackFinal": async (config) => {
+  'webpackFinal': async (config) => {
     config.module.rules.push({
       test: /\.css$/,
       use: [
         {
-          loader: "postcss-loader",
+          loader: 'postcss-loader',
           options: {
-            plugins: [
-              require("tailwindcss"),
-              require("autoprefixer"),
-            ],
-          },
-        },
+            postcssOptions: {
+              plugins: [
+                require('tailwindcss'),
+                require('autoprefixer')
+              ]
+            }
+          }
+        }
       ],
-      include: path.resolve(__dirname, '../'),
+      include: path.resolve(__dirname, '../')
     })
     return config
-  },
+  }
 }
 
