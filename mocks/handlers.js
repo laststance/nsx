8dc61e0df56986a080e4ac59eafe8cde79f34db9
ryota-murakami <dojce1048@gmail.com>
2022-01-26 22:54:11 +0900

    define mock handler

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,7 +1,28 @@
 import { rest } from 'msw'
 
 export const handlers = [
-  rest.get('/docs', (req, res, ctx) => {
+  rest.get('/post_list', (req, res, ctx) => {
+    return res(ctx.body())
+  }),
+  rest.get('/post/:id', (req, res, ctx) => {
+    return res(ctx.body())
+  }),
+  rest.delete('/post/:id', (req, res, ctx) => {
+    return res(ctx.body())
+  }),
+  rest.post('/signup', (req, res, ctx) => {
+    return res(ctx.body())
+  }),
+  rest.post('/login', (req, res, ctx) => {
+    return res(ctx.body())
+  }),
+  rest.get('/logout', (req, res, ctx) => {
+    return res(ctx.body())
+  }),
+  rest.post('/create', (req, res, ctx) => {
+    return res(ctx.body())
+  }),
+  rest.post('/update', (req, res, ctx) => {
     return res(ctx.body())
   }),
 ]
