1855696d64c6f040d671b58ef6bbe25b43f44ccd
ryota-murakami <dojce1048@gmail.com>
2022-01-29 21:01:00 +0900

    storybook: hide addon panel

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,5 +2,25 @@ import { addons } from '@storybook/addons'
 import { themes } from '@storybook/theming'
 
 addons.setConfig({
-  theme: themes.dark
-})
+    isFullscreen: false,
+    showNav: true,
+    showPanel: false,
+    panelPosition: 'bottom',
+    enableShortcuts: true,
+    isToolshown: true,
+    theme: themes.dark,
+    selectedPanel: undefined,
+    initialActive: 'sidebar',
+    sidebar: {
+      showRoots: true,
+      collapsedRoots: ['other']
+    },
+    toolbar: {
+      title: { hidden: false },
+      zoom: { hidden: false },
+      eject: { hidden: false },
+      copy: { hidden: false },
+      fullscreen: { hidden: false }
+    }
+  }
+)
