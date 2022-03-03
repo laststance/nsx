7f8560b270ed68f07ba34538db48ca95521cada8
ryota-murakami <dojce1048@gmail.com>
2022-03-04 01:52:55 +0900

    fix listbox position

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -204,37 +204,39 @@ export function ThemeToggle({ panelClassName = 'mt-4' }) {
   return (
     <Listbox value={setting} onChange={setSetting}>
       <Listbox.Label className="sr-only">Theme</Listbox.Label>
-      <Listbox.Button type="button">
-        <span className="dark:hidden">
-          <SunIcon className="w-6 h-6" selected={setting !== 'system'} />
-        </span>
-        <span className="dark:inline hidden">
-          <MoonIcon className="w-6 h-6" selected={setting !== 'system'} />
-        </span>
-      </Listbox.Button>
-      <Listbox.Options
-        className={clsx(
-          'absolute z-50 bg-white rounded-lg ring-1 ring-slate-900/10 shadow-lg overflow-hidden w-36 py-1 text-sm text-slate-700 font-semibold dark:bg-neutral-700 dark:ring-0 dark:highlight-white/5 dark:text-slate-300',
-          panelClassName
-        )}
-      >
-        {settings.map(({ value, label, icon: Icon }) => (
-          <Listbox.Option key={value} value={value} as={Fragment}>
-            {({ active, selected }) => (
-              <li
-                className={clsx(
-                  'py-1 px-2 flex items-center cursor-pointer',
-                  selected && 'text-green-400',
-                  active && 'bg-slate-50 dark:bg-slate-600/30'
-                )}
-              >
-                <Icon selected={selected} className="w-6 h-6 mr-2" />
-                {label}
-              </li>
-            )}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
+      <div className="relative">
+        <Listbox.Button type="button">
+          <span className="dark:hidden">
+            <SunIcon className="w-6 h-6" selected={setting !== 'system'} />
+          </span>
+          <span className="dark:inline hidden">
+            <MoonIcon className="w-6 h-6" selected={setting !== 'system'} />
+          </span>
+        </Listbox.Button>
+        <Listbox.Options
+          className={clsx(
+            'absolute top-5 -right-16 z-50 bg-white rounded-lg ring-1 ring-slate-900/10 shadow-lg overflow-hidden w-36 py-1 text-sm text-slate-700 font-semibold dark:bg-neutral-700 dark:ring-0 dark:highlight-white/5 dark:text-slate-300',
+            panelClassName
+          )}
+        >
+          {settings.map(({ value, label, icon: Icon }) => (
+            <Listbox.Option key={value} value={value} as={Fragment}>
+              {({ active, selected }) => (
+                <li
+                  className={clsx(
+                    'py-1 px-2 flex items-center cursor-pointer',
+                    selected && 'text-green-400',
+                    active && 'bg-slate-50 dark:bg-slate-600/30'
+                  )}
+                >
+                  <Icon selected={selected} className="w-6 h-6 mr-2" />
+                  {label}
+                </li>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </div>
     </Listbox>
   )
 }
