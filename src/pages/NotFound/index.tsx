e46d8af4adca44be9d911961d53e89b03b87ff7b
ryota-murakami <dojce1048@gmail.com>
2022-04-15 20:30:40 +0900

    set text-color for Notfound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -8,7 +8,7 @@ const NotFound = memo(
       className="flex items-center justify-center"
       data-cy="page-notfound"
     >
-      <h1 className="text-6xl">404: Page Not Found</h1>
+      <h1 className="text-color-primary text-6xl">404: Page Not Found</h1>
     </Layout>
   ),
   () => true
