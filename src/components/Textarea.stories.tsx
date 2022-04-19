1262ecae32ad7c8ee4fa53087d16bbbabef1caec
ryota-murakami <dojce1048@gmail.com>
2022-04-19 20:00:49 +0900

    add <Textarea /> to Storybook

---
+++ src/components/Textarea.stories.tsx
@@ -0,0 +1,36 @@
+import type { ComponentStory } from '@storybook/react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+import Textarea from './Textarea'
+
+export default {
+  title: 'Components/Textarea',
+  component: Textarea,
+}
+
+interface FormInput {
+  message: string
+}
+
+const TextareaForm = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm<FormInput>()
+  return (
+    <Textarea
+      register={register}
+      name="message"
+      options={{ required: 'message is required' }}
+      errors={errors}
+      placeholder="Type any text"
+    />
+  )
+}
+
+const Template: ComponentStory<typeof Textarea> = (props) => (
+  <TextareaForm {...props} />
+)
+
+export const Default = Template.bind({})
