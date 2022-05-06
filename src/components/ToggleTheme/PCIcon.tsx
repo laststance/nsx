83e5752154bcff1336923af1a9e69d3d1a734aa6
ryota-murakami <dojce1048@gmail.com>
2022-05-07 08:15:17 +0900

    Icon memo

--- src/components/ToggleTheme/PCIcon.tsx
+++ src/components/ToggleTheme/PCIcon.tsx
@@ -1,34 +1,36 @@
 import type { ComponentProps } from 'react'
-import React from 'react'
+import React, { memo } from 'react'
 
 interface Props {
   selected: boolean
 }
 
-export function PcIcon({ selected, ...props }: Props & ComponentProps<'svg'>) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" {...props}>
-      <path
-        d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6Z"
-        strokeWidth="2"
-        strokeLinejoin="round"
-        className={
-          selected
-            ? 'fill-neutral-400 stroke-neutral-400'
-            : 'stroke-slate-400 dark:stroke-slate-500'
-        }
-      />
-      <path
-        d="M14 15c0 3 2 5 2 5H8s2-2 2-5"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={
-          selected
-            ? 'stroke-neutral-400'
-            : 'stroke-slate-400 dark:stroke-slate-500'
-        }
-      />
-    </svg>
-  )
-}
+export const PcIcon = memo(
+  ({ selected, ...props }: Props & ComponentProps<'svg'>) => {
+    return (
+      <svg viewBox="0 0 24 24" fill="none" {...props}>
+        <path
+          d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6Z"
+          strokeWidth="2"
+          strokeLinejoin="round"
+          className={
+            selected
+              ? 'fill-neutral-400 stroke-neutral-400'
+              : 'stroke-slate-400 dark:stroke-slate-500'
+          }
+        />
+        <path
+          d="M14 15c0 3 2 5 2 5H8s2-2 2-5"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className={
+            selected
+              ? 'stroke-neutral-400'
+              : 'stroke-slate-400 dark:stroke-slate-500'
+          }
+        />
+      </svg>
+    )
+  }
+)
