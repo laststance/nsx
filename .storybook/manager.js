6c2f13b83655ea90845960b0b4f68c41dd6987ef
ryota-murakami <dojce1048@gmail.com>
2022-05-14 08:40:34 +0900

    update storybook config

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,26 +1,26 @@
 import { addons } from '@storybook/addons'
 import { themes } from '@storybook/theming'
 
-// addons.setConfig({
-//     isFullscreen: false,
-//     showNav: true,
-//     showPanel: false,
-//     panelPosition: 'bottom',
-//     enableShortcuts: true,
-//     isToolshown: true,
-//     theme: themes.dark,
-//     selectedPanel: undefined,
-//     initialActive: 'sidebar',
-//     sidebar: {
-//       showRoots: true,
-//       collapsedRoots: ['other']
-//     },
-//     toolbar: {
-//       title: { hidden: false },
-//       zoom: { hidden: false },
-//       eject: { hidden: false },
-//       copy: { hidden: false },
-//       fullscreen: { hidden: false }
-//     }
-//   }
-// )
+addons.setConfig({
+    isFullscreen: false,
+    showNav: true,
+    showPanel: true,
+    panelPosition: 'bottom',
+    enableShortcuts: true,
+    isToolshown: true,
+    theme: themes.dark,
+    selectedPanel: undefined,
+    initialActive: 'sidebar',
+    sidebar: {
+      showRoots: true,
+      collapsedRoots: ['other']
+    },
+    toolbar: {
+      title: { hidden: false },
+      zoom: { hidden: false },
+      eject: { hidden: false },
+      copy: { hidden: false },
+      fullscreen: { hidden: false }
+    }
+  }
+)
