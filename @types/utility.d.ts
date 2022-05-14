3cadb32bf4a1556ebbeb4e3b692463b4043e8930
ryota-murakami <dojce1048@gmail.com>
2022-05-14 09:34:27 +0900

    remove unused utility type

--- @types/utility.d.ts
+++ @types/utility.d.ts
@@ -37,25 +37,6 @@ declare type Primitive =
   | bigint
 
 /**
- Matches a [`class`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes).
-
- @category Basic
- */
-declare type Class<T, Arguments extends unknown[] = any[]> = Constructor<
-  T,
-  Arguments
-> & { prototype: T }
-
-/**
- Matches a [`class` constructor](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes).
-
- @category Basic
- */
-declare type Constructor<T, Arguments extends unknown[] = any[]> = new (
-  ...arguments_: Arguments
-) => T
-
-/**
  Matches a JSON object.
 
  This type can be useful to enforce some input to be JSON-compatible or as a super-type to be extended from. Don't use this as a direct return type as the user would have to double-cast it: `jsonObject as unknown as CustomResponse`. Instead, you could extend your CustomResponse type from it to ensure your type only uses JSON-compatible types: `interface CustomResponse extends JsonObject { … }`.
