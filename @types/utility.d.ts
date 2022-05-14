9cba65f7c2653f53d13a04931b90be09ce193ac6
ryota-murakami <dojce1048@gmail.com>
2022-05-14 09:31:28 +0900

    remove unused utility type

--- @types/utility.d.ts
+++ @types/utility.d.ts
@@ -4,10 +4,6 @@ declare type Override<T1, T2> = T2 extends any ? Omit<T1, keyof T2> & T2 : never
 
 declare type NoInfer<T> = [T][T extends any ? 0 : never]
 
-declare type OmitFromUnion<T, K extends keyof T> = T extends any
-  ? Omit<T, K>
-  : never
-
 declare type IsAny<T, True, False = never> = true | false extends (
   T extends never ? true : false
 )
