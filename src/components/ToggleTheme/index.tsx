3d7b9aa111dba0b721b98276f67d22749cff91c0
ryota-murakami <dojce1048@gmail.com>
2022-05-16 10:12:08 +0900

    remove unused props

--- src/components/ToggleTheme/index.tsx
+++ src/components/ToggleTheme/index.tsx
@@ -25,50 +25,43 @@ const options = [
   },
 ]
 
-const ThemeToggle = memo<{ panelClassName?: string }>(
-  ({ panelClassName = 'mt-4' }) => {
-    const [theme, updateTheme] = useTheme()
+const ThemeToggle = memo(() => {
+  const [theme, updateTheme] = useTheme()
 
-    return (
-      <Listbox value={theme} onChange={(v) => updateTheme(v)}>
-        <Listbox.Label className="sr-only">Theme</Listbox.Label>
-        <div className="relative">
-          <Listbox.Button type="button">
-            <span className="dark:hidden">
-              <SunIcon className="h-6 w-6" selected={theme !== 'system'} />
-            </span>
-            <span className="hidden dark:inline">
-              <MoonIcon className="h-6 w-6" selected={theme !== 'system'} />
-            </span>
-          </Listbox.Button>
-          <Listbox.Options
-            className={clsx(
-              'dark:highlight-white/5 absolute top-5 -right-16 z-50 w-36 overflow-hidden rounded-lg bg-white py-1 text-sm font-semibold text-slate-700 shadow-lg ring-1 ring-slate-900/10 dark:bg-neutral-700 dark:text-slate-300 dark:ring-0',
-              panelClassName
-            )}
-          >
-            {options.map(({ value, label, icon: Icon }) => (
-              <Listbox.Option key={value} value={value} as={Fragment}>
-                {({ active, selected }) => (
-                  <li
-                    className={clsx(
-                      'flex cursor-pointer items-center py-1 px-2',
-                      selected && 'text-green-400',
-                      active && 'bg-slate-50 dark:bg-slate-600/30'
-                    )}
-                  >
-                    <Icon selected={selected} className="mr-2 h-6 w-6" />
-                    {label}
-                  </li>
-                )}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </div>
-      </Listbox>
-    )
-  }
-)
+  return (
+    <Listbox value={theme} onChange={(v) => updateTheme(v)}>
+      <Listbox.Label className="sr-only">Theme</Listbox.Label>
+      <div className="relative">
+        <Listbox.Button type="button">
+          <span className="dark:hidden">
+            <SunIcon className="h-6 w-6" selected={theme !== 'system'} />
+          </span>
+          <span className="hidden dark:inline">
+            <MoonIcon className="h-6 w-6" selected={theme !== 'system'} />
+          </span>
+        </Listbox.Button>
+        <Listbox.Options className="dark:highlight-white/5 absolute top-5 -right-16 z-50 mt-4 w-36 overflow-hidden rounded-lg bg-white py-1 text-sm font-semibold text-slate-700 shadow-lg ring-1 ring-slate-900/10 dark:bg-neutral-700 dark:text-slate-300 dark:ring-0">
+          {options.map(({ value, label, icon: Icon }) => (
+            <Listbox.Option key={value} value={value} as={Fragment}>
+              {({ active, selected }) => (
+                <li
+                  className={clsx(
+                    'flex cursor-pointer items-center py-1 px-2',
+                    selected && 'text-green-400',
+                    active && 'bg-slate-50 dark:bg-slate-600/30'
+                  )}
+                >
+                  <Icon selected={selected} className="mr-2 h-6 w-6" />
+                  {label}
+                </li>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </div>
+    </Listbox>
+  )
+})
 ThemeToggle.displayName = 'ThemeToggle'
 
 export default ThemeToggle
