476154e188c0b89567382070614538d928e31664
ryota-murakami <dojce1048@gmail.com>
2022-05-22 04:59:33 +0900

    add Dashboard.test.tsx

--- src/pages/Dashboard/Dashboard.test.tsx
+++ src/pages/Dashboard/Dashboard.test.tsx
@@ -1,13 +1,20 @@
+import { waitFor, getByRole } from '@testing-library/dom'
 import React from 'react'
 
+import { sleep } from '../../../lib/sleep'
 import TestRenderer from '../../lib/TestRenderer'
 
 import Dashboard from './'
 
-test('should render Dashboard', () => {
+test('should render Dashboard', async () => {
   const {
     container: { firstChild },
   } = TestRenderer(<Dashboard />)
-  expect(firstChild).toBeTruthy()
-  expect(firstChild).toMatchSnapshot()
+
+  await waitFor(async () => {
+    await sleep(500) // wait for loading
+    expect(firstChild).toBeTruthy()
+    const main = getByRole(firstChild as HTMLElement, 'main')
+    expect(getByRole(main, 'heading')).toHaveTextContent('Dashboard')
+  })
 })
