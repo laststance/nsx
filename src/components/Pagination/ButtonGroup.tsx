d475616d748cf88057c357ff439e39961feada0d
ryota-murakami <dojce1048@gmail.com>
2022-05-21 12:04:29 +0900

    refactor button group

--- src/components/Pagination/ButtonGroup.tsx
+++ src/components/Pagination/ButtonGroup.tsx
@@ -2,8 +2,8 @@ import React, { memo } from 'react'
 
 import PageCount from '../../pages/Index/PageCount'
 import type { PagenationState } from '../../redux/pagenationSlice'
-import { updatePage } from '../../redux/pagenationSlice'
-import { dispatch } from '../../redux/store'
+import { updatePage, selectPagenation } from '../../redux/pagenationSlice'
+import { dispatch, store } from '../../redux/store'
 import ArrowButton from '../ArrowButton'
 
 import type { UsePagenationResult } from './usePagination'
@@ -13,11 +13,13 @@ interface Props {
   totalPage: UsePagenationResult['totalPage']
 }
 
-const prevPage = (page: PagenationState['page']) => {
-  return () => dispatch(updatePage({ page: page - 1 }))
+const prevPage = () => {
+  const { page } = selectPagenation(store.getState())
+  dispatch(updatePage({ page: page - 1 }))
 }
-const nextPage = (page: PagenationState['page']) => {
-  return () => dispatch(updatePage({ page: page + 1 }))
+const nextPage = () => {
+  const { page } = selectPagenation(store.getState())
+  dispatch(updatePage({ page: page + 1 }))
 }
 
 const ButtonGroup: React.FC<React.PropsWithChildren<Props>> = memo(
@@ -25,7 +27,7 @@ const ButtonGroup: React.FC<React.PropsWithChildren<Props>> = memo(
     <div className="flex items-center justify-center space-x-4 p-8 px-10">
       <ArrowButton
         direction="left"
-        onClick={prevPage(page)}
+        onClick={prevPage}
         disabled={page <= 1 ? true : false}
         data-cy="prev-page-btn"
       />
@@ -37,7 +39,7 @@ const ButtonGroup: React.FC<React.PropsWithChildren<Props>> = memo(
       />
       <ArrowButton
         direction="right"
-        onClick={nextPage(page)}
+        onClick={nextPage}
         disabled={page === totalPage ? true : false}
         data-cy="next-page-btn"
       />
