1c928a949f8f6c27512909f8db35805ee0b97e1f
ryota-murakami <dojce1048@gmail.com>
2022-05-22 12:04:54 +0900

    Signup.test.tsx

--- src/pages/Signup/Signup.test.tsx
+++ src/pages/Signup/Signup.test.tsx
@@ -1,3 +1,4 @@
+import { getByRole } from '@testing-library/dom'
 import React from 'react'
 
 import TestRenderer from '../../lib/TestRenderer'
@@ -7,4 +8,13 @@ import Signup from './'
 test('should render Signup', () => {
   const { container } = TestRenderer(<Signup />)
   expect(container).toBeInTheDocument()
+})
+
+test('should show Signup page', () => {
+  const { container } = TestRenderer(<Signup />)
+  const main = getByRole(container, 'main')
+  expect(getByRole(main, 'heading')).toHaveTextContent('Signup')
+  expect(main.querySelector('[data-cy="name-input"]')).toBeInTheDocument()
+  expect(main.querySelector('[data-cy="password-input"]')).toBeInTheDocument()
+  expect(getByRole(main, 'button')).toHaveTextContent('Submit')
 })
