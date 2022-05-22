e5212edf3cb988364a3ec5cbc8d6c0ba71d84c9c
ryota-murakami <dojce1048@gmail.com>
2022-05-22 11:41:33 +0900

    remove snap

--- src/pages/Signup/Signup.test.tsx
+++ src/pages/Signup/Signup.test.tsx
@@ -5,9 +5,6 @@ import TestRenderer from '../../lib/TestRenderer'
 import Signup from './'
 
 test('should render Signup', () => {
-  const {
-    container: { firstChild },
-  } = TestRenderer(<Signup />)
-  expect(firstChild).toBeTruthy()
-  expect(firstChild).toMatchSnapshot()
+  const { container } = TestRenderer(<Signup />)
+  expect(container).toBeInTheDocument()
 })
