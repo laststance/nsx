0efb575a2dd8d3378f659ea45a4e528e4e7acb83
ryota-murakami <dojce1048@gmail.com>
2022-05-22 19:43:14 +0900

    remove @testing-library/react-hooks

--- src/hooks/useDidUpdateEffect.test.ts
+++ src/hooks/useDidUpdateEffect.test.ts
@@ -1,9 +1,8 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react'
 
 import useDidUpdateEffect from './useDidUpdateEffect'
 
-// @TODO Skip until @testing-library/react-hooks support React 18
-test.skip('useUpdateEffect simulates componentDidUpdate', () => {
+test('useUpdateEffect simulates componentDidUpdate', () => {
   const effect = jest.fn()
   const { rerender } = renderHook(() => useDidUpdateEffect(effect))
 
