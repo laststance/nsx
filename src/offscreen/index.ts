d5ba24564eb90e41846f279484fe576118d60a31
ryota-murakami <dojce1048@gmail.com>
2022-05-31 20:50:36 +0900

    up

--- src/offscreen/index.ts
+++
@@ -1,8 +0,0 @@
-import Permission from './Permission'
-import Redirect from './Redirect'
-import Sidebar from './Sidebar/Sidebar'
-import SnackBarDispatcher from './SnackBarDsipatcher'
-
-const OffScreen = { Permission, Redirect, Sidebar, SnackBarDispatcher }
-
-export default OffScreen
