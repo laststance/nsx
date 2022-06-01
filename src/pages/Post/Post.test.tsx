c31c613ba1940e9a2755d9994e4e6f3ccc191f57
ryota-murakami <dojce1048@gmail.com>
2022-06-01 23:08:03 +0900

    up

--- src/pages/Post/Post.test.tsx
+++ src/pages/Post/Post.test.tsx
@@ -1,27 +1,65 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { waitFor } from '@testing-library/dom'
 import React from 'react'
-// import { Routes, Route } from 'react-router-dom'
-//
-// import { sleep } from '../../../lib/sleep'
-// import TestRenderer from '../../lib/TestRenderer'
-//
-// import Post from './'
+import { Routes, Route } from 'react-router-dom'
 
-// @TODO react-markdown is distributed by ESM, but Jest does not support ESM at the time.
-// I'll activate this test after Jest supported ESM by default.
-//  https://github.com/remarkjs/react-markdown/issues/635#issuecomment-900671613
-test.skip('should render Post', async () => {
-  // const {
-  //   container: { firstChild },
-  // } = TestRenderer(
-  //   <Routes>
-  //     <Route path="/post/:postId" element={<Post />} />
-  //   </Routes>,
-  //   {
-  //     initialEntries: ['/post/2'],
-  //   }
-  // )
-  // await sleep(500)
-  // expect(firstChild).toBeTruthy()
-  // expect(firstChild).toMatchSnapshot()
+import { sleep } from '../../../lib/sleep'
+import TestRenderer from '../../lib/TestRenderer'
+
+import Post from './'
+
+test('should render Post', async () => {
+  const { container, getByRole } = TestRenderer(
+    <Routes>
+      <Route path="/post/:postId" element={<Post />} />
+    </Routes>,
+    {
+      initialEntries: ['/post/2'],
+    }
+  )
+
+  await waitFor(async () => {
+    await sleep(300)
+    expect(container).toBeInTheDocument()
+    const main = getByRole('main')
+    expect(main).toMatchInlineSnapshot(`
+<main
+  class="container mx-auto flex-grow px-4 py-4"
+>
+  <h1
+    class="text-color-primary pt-4 pb-6 text-2xl font-semibold"
+  >
+    superstruct
+  </h1>
+  is masterpiece of validation library ever.
+
+\`\`\`js
+import { is, define, object, string } from 'superstruct'
+import isUuid from 'is-uuid'
+import isEmail from 'is-email'
+
+const Email = define('Email', isEmail)
+const Uuid = define('Uuid', isUuid.v4)
+
+const User = object({
+  id: Uuid,
+  email: Email,
+  name: string(),
+})
+
+const data = {
+  id: 'c8d63140-a1f7-45e0-bfc6-df72973fea86',
+  email: 'jane@example.com',
+  name: 'Jane',
+}
+
+if (is(data, User)) {
+  // Your data is guaranteed to be valid in this block.
+}
+
+\`\`\`
+
+- [ianstormtaylor/superstruct: A simple and composable way to validate data in JavaScript (and TypeScript).](https://github.com/ianstormtaylor/superstruct)
+</main>
+`)
+  })
 })
