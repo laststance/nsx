e6b733f696a0801a970072316c05c869e9b4ebfe
ryota-murakami <dojce1048@gmail.com>
2022-06-06 06:24:54 +0900

    up

--- src/offscreen/Sidebar/LogoutLink.tsx
+++ src/offscreen/Sidebar/LogoutLink.tsx
@@ -4,19 +4,22 @@ import { useNavigate } from 'react-router-dom'
 
 import { handleLogout } from '../../handler'
 
-const LogoutLink: React.FC = memo(() => {
-  const navigate = useNavigate()
-  return (
-    <button
-      onClick={(e) => handleLogout(e, navigate)}
-      data-cy="logout-link"
-      className="group flex items-center rounded-md bg-gray-900 px-2 py-2 text-base font-medium text-white"
-    >
-      <LogoutIcon className="mr-4 h-6 w-6 flex-shrink-0 text-gray-300" />
-      Logout
-    </button>
-  )
-})
+const LogoutLink: React.FC = memo(
+  () => {
+    const navigate = useNavigate()
+    return (
+      <button
+        onClick={(e) => handleLogout(e, navigate)}
+        data-cy="logout-link"
+        className="group flex w-full items-center rounded-md bg-gray-900 px-2 py-2 text-base font-medium text-white"
+      >
+        <LogoutIcon className="mr-4 h-6 w-6 flex-shrink-0 text-gray-300" />
+        Logout
+      </button>
+    )
+  },
+  () => true
+)
 LogoutLink.displayName = 'LogoutLink'
 
 export default LogoutLink
