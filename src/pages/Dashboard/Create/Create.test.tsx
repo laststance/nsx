c06c668857bd204fd81443ec72f765221f0e7c87
ryota-murakami <dojce1048@gmail.com>
2022-06-08 18:43:03 +0900

    up

--- src/pages/Dashboard/Create/Create.test.tsx
+++ src/pages/Dashboard/Create/Create.test.tsx
@@ -4,12 +4,9 @@ import TestRenderer from '../../../lib/TestRenderer'
 
 import Create from './index'
 
-test('should render Create', () => {
-  const {
-    container: { firstChild },
-    getAllByRole,
-  } = TestRenderer(<Create />)
-  expect(firstChild).toBeTruthy()
+test('should render Create Page', () => {
+  const { container, getAllByRole } = TestRenderer(<Create />)
+  expect(container).toBeInTheDocument()
   const [input, textarea] = getAllByRole('textbox')
   expect(input).toBeInTheDocument()
   expect(textarea).toBeInTheDocument()
