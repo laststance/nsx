6f52974684ac4c61c32a45783b8ce351320096f9
ryota-murakami <dojce1048@gmail.com>
2022-06-08 19:17:03 +0900

    move logoutHandler

--- src/offscreen/Sidebar/LogoutLink.tsx
+++ src/offscreen/Sidebar/LogoutLink.tsx
@@ -1,8 +1,27 @@
 import { LogoutIcon } from '@heroicons/react/outline'
 import React, { memo } from 'react'
 import { useNavigate } from 'react-router-dom'
+import type { NavigateFunction } from 'react-router/lib/hooks'
 
-import { handleLogout } from '../../handler'
+import { logout } from '../../redux/adminSlice'
+import { API } from '../../redux/API'
+import isSuccess from '../../redux/helper/isSuccess'
+import { enqueSnackbar } from '../../redux/snackbarSlice'
+import { dispatch } from '../../redux/store'
+
+export async function handleLogout(
+  e: React.MouseEvent<HTMLButtonElement>,
+  navigate: NavigateFunction | undefined = undefined
+) {
+  e.preventDefault()
+  const response = await dispatch(API.endpoints.logoutRequest.initiate())
+
+  if (isSuccess(response) && 'data' in response) {
+    dispatch(logout())
+    dispatch(enqueSnackbar({ color: 'green', message: response.data.message }))
+    if (navigate) navigate('/')
+  }
+}
 
 const LogoutLink: React.FC = memo(
   () => {
