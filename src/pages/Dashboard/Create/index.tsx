8121f6183bc0b4718fc7ba8aac2e4fe5791e66a9
ryota-murakami <dojce1048@gmail.com>
2022-06-08 16:44:51 +0900

    prev extension

--- src/pages/Dashboard/Create/index.tsx
+++ src/pages/Dashboard/Create/index.tsx
@@ -1,7 +1,6 @@
 import { superstructResolver } from '@hookform/resolvers/superstruct'
 import React, { memo } from 'react'
 import { useForm } from 'react-hook-form'
-import type { UseFormSetValue } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 
 import { createPostFormValidator } from '../../../../validator/index'
@@ -10,102 +9,58 @@ import Input from '../../../components/Input/Input'
 import Layout from '../../../components/Layout/index'
 import Textarea from '../../../components/Textarea/Textarea'
 import { selectAuthor } from '../../../redux/adminSlice'
-import {
-  API,
-  useGetStockListQuery,
-  useDeleteStockMutation,
-} from '../../../redux/API'
+import { API } from '../../../redux/API'
 import { selectBody, selectTitle } from '../../../redux/draftSlice'
 import type { FormInput } from '../../../redux/draftSlice'
 import { useAppSelector } from '../../../redux/hooks'
 
 import { handleBodyChange, handleTitleChange, onSubmit } from './handlers'
 
-const Create: React.FC<any> = memo(() => {
-  // @ts-ignore
-  const { data } = useGetStockListQuery()
-  const [deleteStock] = useDeleteStockMutation()
+const Create: React.FC = memo(() => {
   const navigate = useNavigate()
   const [createPost, { isLoading }] = API.endpoints.createPost.useMutation()
   const title = useAppSelector(selectTitle)
   const body = useAppSelector(selectBody)
   const author = useAppSelector(selectAuthor)
 
-  const onClickHandler = async (
-    id: Stock['id'],
-    pageTitle: Stock['pageTitle'],
-    url: Stock['url'],
-    setValue: UseFormSetValue<any>
-  ) => {
-    // @TODO insert as a link to markdown edit
-    const res = await deleteStock(id)
-    setValue('body', pageTitle + url)
-
-    // eslint-disable-next-line no-console
-    console.log(res)
-  }
-
   const {
     register,
     handleSubmit,
-    setValue,
     formState: { errors },
   } = useForm<FormInput>({
     resolver: superstructResolver(createPostFormValidator),
   })
 
   return (
-    <div className="flex w-full gap-4">
-      <form
-        className="flex-grow-1 w-[70%] flex-shrink-0"
-        onSubmit={handleSubmit(() =>
-          onSubmit(createPost, title, body, author, navigate)
-        )}
-      >
-        <Input
-          defaultValue={title}
-          type="text"
-          reactHookFormPrams={{ errors, name: 'title', register }}
-          onChange={handleTitleChange}
-          data-cy="post-title-input"
-        />
-        <Textarea
-          defaultValue={body}
-          reactHookFormParams={{ errors, name: 'body', register }}
-          className="mt-3 h-96 w-full"
-          onChange={handleBodyChange}
-        />
-        <div className="flex justify-end gap-4 pt-8">
-          <Button
-            type="submit"
-            variant="primary"
-            isLoading={isLoading}
-            data-cy="submit-btn"
-          >
-            Submit
-          </Button>
-        </div>
-      </form>
-      <section className="text-color-inverse flex-grow-1 w-[400px] flex-shrink-0">
-        <ul className="w-full">
-          {data &&
-            data.map((v: Stock, i: number) => {
-              return (
-                // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
-                <li
-                  onClick={() =>
-                    onClickHandler(v.id, v.pageTitle, v.url, setValue)
-                  }
-                  className="mb-4 flex h-[64px] items-center overflow-y-scroll rounded-xl bg-green-300 bg-opacity-70 p-4 shadow-2xl hover:bg-opacity-100"
-                  key={i}
-                >
-                  {v.pageTitle}
-                </li>
-              )
-            })}
-        </ul>
-      </section>
-    </div>
+    <form
+      onSubmit={handleSubmit(() =>
+        onSubmit(createPost, title, body, author, navigate)
+      )}
+    >
+      <Input
+        defaultValue={title}
+        type="text"
+        reactHookFormPrams={{ errors, name: 'title', register }}
+        onChange={handleTitleChange}
+        data-cy="post-title-input"
+      />
+      <Textarea
+        defaultValue={body}
+        reactHookFormParams={{ errors, name: 'body', register }}
+        className="mt-3 h-96 w-full"
+        onChange={handleBodyChange}
+      />
+      <div className="flex justify-end gap-4 pt-8">
+        <Button
+          type="submit"
+          variant="primary"
+          isLoading={isLoading}
+          data-cy="submit-btn"
+        >
+          Submit
+        </Button>
+      </div>
+    </form>
   )
 })
 Create.displayName = 'Create'
