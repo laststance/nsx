b3a13aea6be0fcd9dcee29ef24aba8484af7e026
ryota-murakami <dojce1048@gmail.com>
2022-06-08 19:12:20 +0900

    up

--- src/pages/About/About.test.tsx
+++ src/pages/About/About.test.tsx
@@ -4,11 +4,8 @@ import TestRenderer from '../../lib/TestRenderer'
 
 import About from './'
 
-test('should render About', () => {
-  const {
-    container: { firstChild },
-    getAllByRole,
-  } = TestRenderer(<About />)
-  expect(firstChild).toBeTruthy()
+test('should render About Page', () => {
+  const { container, getAllByRole } = TestRenderer(<About />)
+  expect(container).toBeInTheDocument()
   expect(getAllByRole('heading')[1]).toHaveTextContent('About')
 })
