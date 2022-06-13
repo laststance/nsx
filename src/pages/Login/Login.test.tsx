959b0a9d369df89f71ae70e181f1f1c3e61c4a3d
ryota-murakami <dojce1048@gmail.com>
2022-06-14 04:45:50 +0900

    up

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -5,13 +5,10 @@ import TestRenderer from '../../lib/TestRenderer'
 import Login from './'
 
 test('should render Login', () => {
-  const {
-    container: { firstChild },
-    getAllByRole,
-  } = TestRenderer(<Login />)
+  const { container, getAllByRole } = TestRenderer(<Login />)
 
   const h1 = getAllByRole('heading')
-  expect(firstChild).toBeTruthy()
+  expect(container).toBeInTheDocument()
   expect(h1[0]).toHaveTextContent('Today I Learned')
   expect(h1[1]).toHaveTextContent('Login')
 })
