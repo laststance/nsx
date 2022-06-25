cca6e21fabf313aa16f0c3cbca4ba5152010e057
ryota-murakami <dojce1048@gmail.com>
2022-06-25 09:50:09 +0900

    remove sass

--- browser-extension/webpack/base.js
+++ browser-extension/webpack/base.js
@@ -93,8 +93,8 @@ const config = {
         use: ['@svgr/webpack'],
       },
       {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.pcss$/i,
