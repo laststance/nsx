1f3140b576633363fea7dfa6fdf9f6a4abd79125
ryota-murakami <dojce1048@gmail.com>
2022-07-05 21:25:01 +0900

    remove useAppDispatch()

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,7 +1,6 @@
 import type { TypedUseSelectorHook } from 'react-redux'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-import type { AppDispatch, RootState } from './store'
+import type { RootState } from './store'
 
-export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
