c2639f0cc534f3b3b63781dfb8cf282c302e98e1
ryota-murakami <dojce1048@gmail.com>
2022-07-07 23:48:40 +0900

    Post title layout

--- src/pages/Post/Content.tsx
+++ src/pages/Post/Content.tsx
@@ -27,7 +27,7 @@ const Content: React.FC<Props & ComponentProps<any>> = memo(({ post }) => {
       {/* Suspence for lazyload expesive <code /> component */}
 
       <Helment post={post} />
-      <h1 className="text-color-primary pt-4 pb-6 text-2xl font-semibold">
+      <h1 className="text-color-primary pt-4 pb-10 text-2xl font-semibold">
         {post.title}
       </h1>
       <article>
