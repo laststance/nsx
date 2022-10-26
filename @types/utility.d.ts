7142d81e0f09e88ba63a6274ec648ad9679baf8a
ryota-murakami <dojce1048@gmail.com>
2022-10-26 21:15:36 +0900

    remove duplicate type

--- @types/utility.d.ts
+++ @types/utility.d.ts
@@ -27,15 +27,6 @@ declare type Primitive =
   | bigint
 
 /**
- Matches a JSON object.
-
- This type can be useful to enforce some input to be JSON-compatible or as a super-type to be extended from. Don't use this as a direct return type as the user would have to double-cast it: `jsonObject as unknown as CustomResponse`. Instead, you could extend your CustomResponse type from it to ensure your type only uses JSON-compatible types: `interface CustomResponse extends JsonObject { … }`.
-
- @category Basic
- */
-declare type JsonObject = { [Key in string]?: JsonValue }
-
-/**
  Matches a JSON array.
 
  @category Basic
@@ -61,7 +52,7 @@ declare type JsonValue = JsonPrimitive | JsonObject | JsonArray
 /**
  Matches a JSON object.
 
- This type can be useful to enforce some input to be JSON-compatible or as a super-type to be extended from. Don't use this as a direct return type as the user would have to double-cast it: `jsonObject as unknown as CustomResponse`. Instead, you could extend your CustomResponse type from it to ensure your type only uses JSON-compatible types: `interface CustomResponse extends JsonObject { … }`.
+ This type can be usxeful to enforce some input to be JSON-compatible or as a super-type to be extended from. Don't use this as a direct return type as the user would have to double-cast it: `jsonObject as unknown as CustomResponse`. Instead, you could extend your CustomResponse type from it to ensure your type only uses JSON-compatible types: `interface CustomResponse extends JsonObject { … }`.
 
  @category Basic
  */
