cabc001e3fecac47be6ffa4c242ce7fbe01d91ac
ryota-murakami <dojce1048@gmail.com>
2023-03-11 18:31:21 +0900

    replace useEffect to useIsomophicEffect

--- src/pages/Post/Error.tsx
+++ src/pages/Post/Error.tsx
@@ -1,8 +1,9 @@
 import type { SerializedError } from '@reduxjs/toolkit'
 import type { FetchBaseQueryError } from '@reduxjs/toolkit/query'
-import React, { memo, useEffect } from 'react'
+import React, { memo } from 'react'
 
 import { ErrorBoundaryFallbackComponent } from '../../components/ErrorBoundary/ErrorBoundary'
+import { useIsomorphicLayoutEffect } from '../../hooks/useIsomorphicLayoutEffect'
 import { enqueSnackbar } from '../../redux/snackbarSlice'
 import { dispatch } from '../../redux/store'
 
@@ -13,7 +14,7 @@ interface Props<T extends Error> {
 // @TODO replace to render error html rather than snackbar
 const Error: React.FC<React.PropsWithChildren<Props<any>>> = memo(
   ({ error }) => {
-    useEffect(() => {
+    useIsomorphicLayoutEffect(() => {
       if (error)
         dispatch(enqueSnackbar({ color: 'red', message: error.toString() }))
       // @TODO Sentry
