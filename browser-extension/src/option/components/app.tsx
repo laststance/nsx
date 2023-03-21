1bcf67492de2d1a44b9c90e7000108b2e1d520f5
ryota-murakami <dojce1048@gmail.com>
2023-03-21 14:22:00 +0900

    up extension logo

--- browser-extension/src/option/components/app.tsx
+++ browser-extension/src/option/components/app.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 function App() {
-  return <div id="app-root"></div>
+  return <div id="app-root">
+    <a target="_blank" href="https://icons8.com/icon/26090/bookmark">Bookmark</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+  </div>
 }
 
 export default App
