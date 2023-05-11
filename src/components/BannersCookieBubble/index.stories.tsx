88d1d11cdb9f00aa7de0bf639854d214d5da3597
ryota-murakami <dojce1048@gmail.com>
2023-05-11 15:28:20 +0900

    add BannersCookieBubble

---
+++ src/components/BannersCookieBubble/index.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import BannarsCookieBubble from './'
+
+const meta: Meta<typeof BannarsCookieBubble> = {
+  component: BannarsCookieBubble,
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'components/BannarsCookieBubble',
+}
+
+export default meta
+type Story = StoryObj<typeof BannarsCookieBubble>
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: () => <BannarsCookieBubble />,
+}
