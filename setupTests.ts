2f3cc077e91627fe16529f32c668fb33088ec84c
ryota-murakami <dojce1048@gmail.com>
2023-08-24 09:41:33 +0900

    fix test

--- setupTests.ts
+++ setupTests.ts
@@ -1,8 +1,6 @@
-import matchers from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom/vitest'
 import MatchMediaMock from 'vitest-matchmedia-mock'
 
-// extends Vitest's expect method with methods from react-testing-library
-expect.extend(matchers)
 // Polyfill "window.fetch" used in the React component.
 import 'node-fetch'
 import { server } from './mocks/server'
