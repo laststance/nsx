fb1cfae4ea013c4ab3859a1bed538cc38a3371c3
ryota-murakami <dojce1048@gmail.com>
2023-11-09 10:10:11 +0900

    Add new file .storybook/manager.ts and update package.json with new dependencies

---
+++ .storybook/manager.ts
@@ -0,0 +1,24 @@
+import { addons } from '@storybook/manager-api'
+
+addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: 'bottom',
+  enableShortcuts: true,
+  showToolbar: true,
+  theme: undefined,
+  selectedPanel: undefined,
+  initialActive: 'sidebar',
+  sidebar: {
+    showRoots: false,
+    collapsedRoots: ['other'],
+  },
+  toolbar: {
+    title: { hidden: true },
+    zoom: { hidden: true },
+    eject: { hidden: true },
+    copy: { hidden: true },
+    fullscreen: { hidden: true },
+  },
+})
