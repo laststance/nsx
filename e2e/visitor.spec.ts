b895c08468da4d88f56456628efc22917c6e174c
ryota-murakami <dojce1048@gmail.com>
2024-01-27 21:28:57 +0900

    migrate visitor spec

--- e2e/visitor.spec.ts
+++ e2e/visitor.spec.ts
@@ -44,6 +44,108 @@ test.describe('visitor basic', () => {
   })
 })
 
+test.describe('pagenation', () => {
+  test('working pagenation correctly', async ({ page }) => {
+    // showing current & total page number
+    await page.goto('http://localhost:3000/')
+    await expect(page.getByTestId('single-post-page-link-1')).toHaveText(
+      /close your eyes/,
+    )
+    await expect(page.getByTestId('page-count')).toHaveText('1 / 4')
+
+    // Next Page Button
+    await page.getByTestId('next-page-btn').click()
+    await expect(page.getByTestId('single-post-page-link-1')).toHaveText(
+      'Satisfied todays code things',
+    )
+    // expect moved page 2
+    await expect(page.getByTestId('page-count')).toHaveText('2 / 4')
+
+    // working prev button
+    await page.getByTestId('prev-page-btn').click()
+    await expect(page.getByTestId('single-post-page-link-1')).toHaveText(
+      /close your eyes/,
+    )
+    await expect(page.getByTestId('page-count')).toHaveText('1 / 4')
+
+    // disabled prev button at 1st page
+    await expect(page.getByTestId('page-count')).toHaveText('1 / 4')
+    await expect(page.getByTestId('prev-page-btn')).toBeDisabled()
+
+    // disabled next page at last page
+    await page.getByTestId('next-page-btn').click()
+    await expect(page.getByTestId('page-count')).toHaveText('2 / 4')
+
+    await page.getByTestId('next-page-btn').click()
+    await expect(page.getByTestId('page-count')).toHaveText('3 / 4')
+
+    await page.getByTestId('next-page-btn').click()
+    await expect(page.getByTestId('page-count')).toHaveText('4 / 4')
+
+    await expect(page.getByTestId('next-page-btn')).toBeDisabled()
+  })
+})
+
+test.describe('site theme', () => {
+  test('Working Light Theme switching', async ({ page }) => {
+    await page.goto('http://localhost:3000/')
+
+    // Check Light Theme
+    await page.getByTestId('theme-menu-button').click()
+    await page.getByTestId('theme-select-option-light').click()
+    await expect(page.getByTestId('root')).not.toHaveClass('dark')
+    await expect(page.getByTestId('body')).toHaveCSS(
+      'background-color',
+      'rgb(255, 255, 255)',
+    )
+  })
+
+  test('Working Dark Theme switching', async ({ page }) => {
+    await page.goto('http://localhost:3000/')
+    // Check Light Theme
+    await page.getByTestId('theme-menu-button').click()
+    await page.getByTestId('theme-select-option-light').click()
+    await expect(page.getByTestId('root')).not.toHaveClass(/dark/)
+    await expect(page.getByTestId('body')).toHaveCSS(
+      'background-color',
+      'rgb(255, 255, 255)',
+    )
+
+    // Check Dark Theme
+    await page.getByTestId('theme-menu-button').click()
+    await page.getByTestId('theme-select-option-dark').click()
+    await expect(page.getByTestId('root')).toHaveClass(/dark/)
+    await expect(page.getByTestId('body')).toHaveCSS(
+      'background-color',
+      'rgb(23, 23, 23)',
+    )
+  })
+
+  test.describe('security', () => {
+    test('never show private routes without login', async ({ page }) => {
+      await page.goto('http://localhost:3000/')
+
+      await page.goto('http://localhost:3000/login')
+      await expect(page.getByRole('main')).toHaveText(/Login/)
+
+      await page.goto('http://localhost:3000/signup')
+      await expect(page.getByRole('main')).toHaveText('404: Page Not Found')
+
+      await page.goto('http://localhost:3000/dashboard')
+      await expect(page.getByRole('main')).toHaveText('404: Page Not Found')
+
+      await page.goto('http://localhost:3000/create')
+      await expect(page.getByRole('main')).toHaveText('404: Page Not Found')
+
+      await page.goto('http://localhost:3000/edit')
+      await expect(page.getByRole('main')).toHaveText('404: Page Not Found')
+
+      await page.goto('http://localhost:3000/delete')
+      await expect(page.getByRole('main')).toHaveText('404: Page Not Found')
+    })
+  })
+})
+
 test.afterAll(async () => {
   await exec('pnpm db:reset')
 })
--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 6,
+  workers: 1,
   // globalSetup: require.resolve('./e2e/global-setup'),
   // globalTeardown: require.resolve('./e2e/global-teardown'),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
