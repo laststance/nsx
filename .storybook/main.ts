a8fb4be2f982d4d614fdf7fb8a2b3c2ba8435b99
ryota-murakami <dojce1048@gmail.com>
2024-02-28 11:46:51 +0900

    Update Storybook and related addons to v8.0.0-beta.5 add Chromatic
    - Upgraded Storybook and all related addons to version 8.0.0-beta.5.
    - Removed storybookaddon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,8 +5,8 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@chromatic-com/storybook',
   ],
   framework: {
     name: '@storybook/react-vite',
