74cdaa9d413b34f62d2531e5f6d9fcdd5dc6b518
Ryota Murakami <dojce1048@gmail.com>
2025-01-28 11:34:50 +0900

    Update e2e helper and Playwright version
    - Added a CustomFixtures type to the e2e/helper.ts file to define the authenticated fixture.
    - Corrected a typo in the password input from popcoon

--- e2e/helper.ts
+++ e2e/helper.ts
@@ -1,14 +1,19 @@
 // eslint-disable-next-line import/named
 import { test as base } from '@playwright/test'
+import type { Page } from '@playwright/test'
 
-export const test = base.extend({
+type CustomFixtures = {
+  authenticated: Page
+}
+
+export const test = base.extend<CustomFixtures>({
   authenticated: async ({ page }, use) => {
     // Perform login actions
     await page.goto('http://localhost:3000')
     await page.keyboard.press('x')
     await page.getByTestId('login-link').click()
     await page.getByTestId('name-input').fill('John Doe')
-    await page.getByTestId('password-input').fill('popcoon')
+    await page.getByTestId('password-input').fill('popcorn')
     await page.getByTestId('submit-btn').click()
 
     // Pass the logged in page to the test
