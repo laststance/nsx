110912ca946ebc71bc84fcbf18df889fa5300387
Ryota Murakami <dojce1048@gmail.com>
2025-06-14 21:43:07 +0900

    refactor: Rename eslint config file and update references
    - Renamed eslint.config.js to eslint.config.mjs for consistency with module syntax.
    - Updated lint-staged configuration in package.json to use the new eslint

--- src/pages/Post/ReactMarkdown/CostomComponents.tsx
+++ src/pages/Post/ReactMarkdown/CostomComponents.tsx
@@ -2,7 +2,4 @@ import type { ComponentProps, ReactNode } from 'react'
 import type { ExtraProps } from 'react-markdown'
 
 type Props = ComponentProps<'a'> & ExtraProps
-export const a = (props: Props): ReactNode => (
-  // eslint-disable-next-line jsx-a11y/anchor-has-content
-  <a {...props} target="_blank"></a>
-)
+export const a = (props: Props): ReactNode => <a {...props} target="_blank"></a>
